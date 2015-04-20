a3a398b8775fed599f18d7cfb10758bd5215ff8d
Roman Golovin <danteek@gmail.com>
2015-04-20 13:45:30 +0300

    Further sidebar layouting
    Added editorconfig

--- icu/packages/custom/icu/public/components/sidepane/sidepane.directive.js
+++ icu/packages/custom/icu/public/components/sidepane/sidepane.directive.js
@@ -3,36 +3,60 @@
 angular.module('mean.icu.ui.sidepane', []).
 directive('icuSidepane', function() {
   function controller($scope) {
-    $scope.items = [{
-            name: 'Tasks',
-            active: false,
-            icon: 'fa-3x fa-check-square'
-        }, {
-            name: 'Projects',
-            active: false,
-            icon: 'fa-3x fa-briefcase'
+      $scope.items = [{
+          name: 'Tasks',
+          active: false,
+          icon: 'fa-3x fa-check-square'
+      }, {
+          name: 'Projects',
+          active: false,
+          icon: 'fa-3x fa-briefcase'
+      }, {
+          name: 'Meetings',
+          active: false,
+          icon: 'fa-3x fa-comments-o'
+      }, {
+          name: 'People',
+          active: true,
+          icon: 'fa-3x fa-user'
+      }];
 
-        }, {
-            name: 'Meetings',
-            active: false,
-            icon: 'fa-3x fa-comments-o'
+      $scope.projects = [{
+          name: 'ICU',
+          id: 1,
+          color: 'green'
+      }, {
+          name: 'Linnovate',
+          id: 2,
+          color: 'blue'
+      }, {
+          name: 'Pixel',
+          id: 3,
+          color: 'pink'
+      }];
 
-        }, {
-            name: 'People',
-            active: true,
-            icon: 'fa-3x fa-user'
-        }];
+      $scope.discussions = [{
+          name: 'Project review',
+          id: 1,
+          active: true
+      }, {
+          name: 'Weekly review',
+          id: 2,
+          active: false
+      }, {
+          name: 'QBR',
+          id: 3,
+          active: false
+      }];
 
-        $scope.selected = function(item) {
-            $scope.items.forEach(function(i) {
-                i.active = false;
-            });
+      $scope.selected = function(item) {
+          $scope.items.forEach(function(i) {
+              i.active = false;
+          });
 
-            item.active = true;
-        };
+          item.active = true;
+      };
   }
-
-
 
   return {
     restrict: 'A',
