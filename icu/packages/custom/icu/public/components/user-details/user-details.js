34ea84907eb90781395bbc7def1b31febc9203be
Roman Golovin <danteek@gmail.com>
2015-04-22 14:29:42 +0300

    Split styles files

--- icu/packages/custom/icu/public/components/user-details/user-details.js
+++ icu/packages/custom/icu/public/components/user-details/user-details.js
@@ -2,14 +2,5 @@
 
 angular.module('mean.icu.ui.userdetails', [])
 .controller('UserDetailsController', function($scope) {
-    $scope.people = [{
-        name: 'John Doe',
-        id: 1
-    }, {
-        name: 'Idan Arbel',
-        id: 2
-    }, {
-        name: 'Lior Kessos',
-        id: 3
-    }];
+
 });
--- icu/packages/custom/icu/public/components/user-list/user-list.js
+++ icu/packages/custom/icu/public/components/user-list/user-list.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('mean.icu.ui.userlist', [])
-.controller('UserListController', function() {
-
+.controller('UserListController', function($scope) {
+    $scope.people = [{
+        name: 'John Doe',
+        id: 1
+    }, {
+        name: 'Idan Arbel',
+        id: 2
+    }, {
+        name: 'Lior Kessos',
+        id: 3
+    }];
 });
