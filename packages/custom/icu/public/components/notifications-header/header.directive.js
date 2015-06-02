1ff43c4fb0e9049b7d2e12448eabd08b17eec1bc
Roman Golovin <danteek@gmail.com>
2015-06-02 13:47:47 +0300

    Add user menu with logout option

--- packages/custom/icu/public/components/notifications-header/header.directive.js
+++ packages/custom/icu/public/components/notifications-header/header.directive.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('mean.icu.ui.notificationsheader', [])
-.directive('icuNotificationsHeader', function(NotificationsService) {
+.directive('icuNotificationsHeader', function(NotificationsService, UsersService, $state) {
     function controller($scope) {
         $scope.notifications = NotificationsService.getAll();
         $scope.lastNotification = _($scope.notifications).last();
+
+        $scope.logout = function() {
+            UsersService.logout().then(function() {
+                $state.go('login');
+            });
+        }
     }
 
     return {
