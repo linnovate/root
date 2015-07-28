8081d77c930b8aba023a8bc7469c2de8a203dbe8
Ekaterina Lizogubova <lizogubova@wookieelabs.com>
2015-07-28 16:09:47 +0300

    Implement member delete icon

--- packages/custom/icu/public/components/members-footer/members-footer.js
+++ packages/custom/icu/public/components/members-footer/members-footer.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('mean.icu.ui.membersfooter', [])
-.directive('icuMembersFooter', function() {
-    function controller($scope) {
+    .directive('icuMembersFooter', function () {
+        function controller($scope) {
+            $scope.deleteMember = function (member) {
 
-    }
+            };
+        }
 
-    return {
-        restrict: 'A',
-        scope: {
-            members: '='
-        },
-        controller: controller,
-        templateUrl: '/icu/components/members-footer/members-footer.html'
-    };
-});
+        return {
+            restrict: 'A',
+            scope: {
+                members: '='
+            },
+            controller: controller,
+            templateUrl: '/icu/components/members-footer/members-footer.html'
+        };
+    });
