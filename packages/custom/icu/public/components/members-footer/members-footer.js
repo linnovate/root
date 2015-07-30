4391c8533da14c992a01eb8d291564ed4f8e9602
Ekaterina Lizogubova <lizogubova@wookieelabs.com>
2015-07-30 17:45:16 +0300

    Implement members add and delete

--- packages/custom/icu/public/components/members-footer/members-footer.js
+++ packages/custom/icu/public/components/members-footer/members-footer.js
@@ -2,16 +2,48 @@
 
 angular.module('mean.icu.ui.membersfooter', [])
     .directive('icuMembersFooter', function () {
-        function controller($scope) {
-            $scope.deleteMember = function (member) {
+        function controller($scope, $injector, context) {
+            var serviceMap = {
+                projects: 'ProjectsService',
+                discussions: 'DiscussionsService',
+                tasks: 'TasksService'
+            };
 
+            var update = function (entity) {
+                $scope.notAssigned = _.difference($scope.users, $scope.entity.watchers);
+                var serviceName = serviceMap[context.main];
+                var service = $injector.get(serviceName);
+                service.update(entity);
+            };
+            $scope.showSelect = false;
+            $scope.entity.watchers = _.filter($scope.users, function (user) {
+                return $scope.entity.watchers.indexOf(user._id);
+            });
+            $scope.notAssigned = _.difference($scope.users, $scope.entity.watchers);
+
+            $scope.triggerSelect = function () {
+                $scope.showSelect = !$scope.showSelect;
+            };
+
+            $scope.addMember = function (member) {
+                $scope.showSelect = false;
+                $scope.entity.watchers.push(member);
+                update($scope.entity);
+            };
+
+            $scope.deleteMember = function (member) {
+                $scope.entity.watchers = _.reject($scope.entity.watchers, function (mem) {
+                    return _.isEqual(member, mem);
+                });
+                update($scope.entity);
             };
         }
 
         return {
             restrict: 'A',
             scope: {
-                members: '='
+                entity: '=',
+                users: '='
             },
             controller: controller,
             templateUrl: '/icu/components/members-footer/members-footer.html'
