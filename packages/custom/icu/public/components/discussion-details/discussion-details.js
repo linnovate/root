b72b3939f8211bf8024dc3632d8e1d6f7b3c5f8c
Ekaterina Lizogubova <lizogubova@wookieelabs.com>
2015-08-17 14:07:11 +0300

    Implement discussions page
    Discussions by projects
    All discusssions
    Fix routes
    Fix some tasklist bugs

--- packages/custom/icu/public/components/discussion-details/discussion-details.js
+++ packages/custom/icu/public/components/discussion-details/discussion-details.js
@@ -1,27 +1,59 @@
 'use strict';
 
 angular.module('mean.icu.ui.discussiondetails', [])
-    .controller('DiscussionDetailsController', function ($scope, context, $state, DiscussionsService) {
-        $scope.discussion = context.entity;
+    .controller('DiscussionDetailsController', function ($scope, entity, context, $state, DiscussionsService) {
+        $scope.discussion = entity;
 
-        $scope.sendSummary = function () {
-
+        $scope.sendSummary = function (discussion) {
+            DiscussionsService.sendSummary(discussion);
         };
 
-        $scope.star = function () {
+        $scope.statuses = ['Received', 'Completed'];
 
-        };
-
-        $scope.update = function (task) {
-            if (context.entityName === 'discussion') {
-                task.discussion = context.entityId;
+        $scope.$watchGroup(['discussion.description', 'discussion.title'], function (nVal, oVal) {
+            if (nVal !== oVal && oVal) {
+                $scope.delayedUpdate($scope.discussion);
             }
+        });
 
-            DiscussionsService.update(task);
+        $scope.dueOptions = {
+            onSelect: function () {
+                $scope.update($scope.discussion);
+            },
+            dateFormat: 'd.m.yy'
         };
 
-        if ($scope.task &&
-                ($state.current.name === 'main.tasks.byentity')) {
+        $scope.options = {
+            theme: 'bootstrap',
+            buttons: ['bold', 'italic', 'underline', 'anchor', 'quote', 'orderedlist', 'unorderedlist']
+        };
+
+        $scope.star = function (discussion) {
+            DiscussionsService.star(discussion).then(function () {
+                $state.reload('main.tasks.byentity.details');
+            });
+        };
+
+        $scope.deleteDiscussion = function (discussion) {
+            DiscussionsService.remove(discussion._id).then(function () {
+                $state.go('main.tasks.byentity', {
+                    entity: context.entityName,
+                    entityId: context.entityId
+                }, {reload: true});
+            });
+        };
+
+        $scope.update = function (discussion) {
+            DiscussionsService.update(discussion);
+        };
+
+        $scope.delayedUpdate = _.debounce($scope.update, 500);
+
+        if (
+            $scope.discussion && (
+            $state.current.name === 'main.tasks.byentity.details' ||
+            $state.current.name === 'main.discussions.all.details' ||
+            $state.current.name === 'main.discussions.byentity.details')) {
             $state.go('.activities');
         }
     });
