528a44320e6098e2f4d02724b383a658595a3f0b
Ekaterina Lizogubova <lizogubova@wookieelabs.com>
2015-08-24 18:14:10 +0300

    Implement task creation

--- packages/custom/icu/public/components/notifications-header/header.directive.js
+++ packages/custom/icu/public/components/notifications-header/header.directive.js
@@ -5,10 +5,10 @@ angular.module('mean.icu.ui.notificationsheader', [])
                                                TasksService,
                                                UsersService,
                                                $state,
+                                               $stateParams,
                                                context,
                                                ProjectsService,
-                                               DiscussionsService,
-                                               ngDialog) {
+                                               DiscussionsService) {
     function controller($scope) {
         $scope.notifications = NotificationsService.getAll();
         $scope.popupNotifications = $scope.notifications.slice(0, -1);
@@ -30,17 +30,55 @@ angular.module('mean.icu.ui.notificationsheader', [])
             });
         };
 
+        var entities = {
+            projects: 'project',
+            discussions: 'discussion',
+            tasks: 'task'
+        };
+
         $scope.createTask = function () {
-            ngDialog.open({
-                template: '/icu/components/task-create/task-create.html',
-                controller: 'TaskCreateController'
+            var task = {
+                title: '',
+                watchers: [],
+                status: 'New',
+                tags: []
+            };
+
+            var state = 'main.tasks.all.details'; // tasks.all
+            var params = {
+                entity: 'task'
+            };
+
+            if (context.entityName === 'all') {
+                if (context.main === 'tasks') {
+                    // tasks.all
+                    state = 'main.tasks.all.details';
+                    params.entity = 'task';
+                } else {
+                    // discussions.all, projects.all
+                    state = 'main.tasks.byentity.details';
+                    params.entityId = $stateParams.id;
+                    params.entity = entities[context.main];
+                    task[params.entity] = $stateParams.id;
+                }
+            } else {
+                // tasks.projects, tasks.discussions, discussions.projects, projects.discussions
+                state = 'main.tasks.byentity.details';
+                params.entity = $stateParams.entity;
+                params.entityId = $stateParams.entityId;
+                task[$stateParams.entity] = $stateParams.entityId;
+            }
+
+            TasksService.create(task).then(function (result) {
+                params.id = result._id;
+                $state.go(state, params, {reload: true});
             });
         };
 
         $scope.createProject = function () {
             var project = {
                 color: 'b9e67d',
-                title: 'new proj',
+                title: '',
                 watchers: [],
                 status: 'New'
             };
