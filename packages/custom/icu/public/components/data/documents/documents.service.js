02dc9117014dde5f4fa603e252e9cae6eb16bddb
Roman Golovin <danteek@gmail.com>
2015-08-28 14:23:38 +0300

    Fix attachments

--- packages/custom/icu/public/components/data/documents/documents.service.js
+++ packages/custom/icu/public/components/data/documents/documents.service.js
@@ -2,9 +2,40 @@
 
 angular.module('mean.icu.data.documentsservice', [])
     .service('DocumentsService', function ($http, ApiUri, Upload) {
+        var EntityPrefix = '/attachments';
+
+        function getAll() {
+            return $http.get(ApiUri + EntityPrefix).then(function (result) {
+                return result.data;
+            });
+        }
 
         function getById(id) {
-            return $http.get(ApiUri + '/attachments/'/* + id*/).then(function (result) {
+            return $http.get(ApiUri + EntityPrefix + '/' + id).then(function (result) {
+                return result.data;
+            });
+        }
+
+        function getByTaskId(id) {
+            return $http.get(ApiUri + '/tasks/' + id + EntityPrefix).then(function (result) {
+                return result.data;
+            });
+        }
+
+        function getByProjectId(id) {
+            return $http.get(ApiUri + '/projects/' + id + EntityPrefix).then(function (result) {
+                return result.data;
+            });
+        }
+
+        function getByDiscussionId(id) {
+            return $http.get(ApiUri + '/discussions/' + id + EntityPrefix).then(function (result) {
+                return result.data;
+            });
+        }
+
+        function getByUserId(id) {
+            return $http.get(ApiUri + '/users/' + id + EntityPrefix).then(function (result) {
                 return result.data;
             });
         }
@@ -18,13 +49,18 @@ angular.module('mean.icu.data.documentsservice', [])
         }
 
         function updateAttachment(id, data) {
-            return $http.post(ApiUri + '/attachments/' + id, data).then(function (result) {
+            return $http.post(ApiUri + EntityPrefix + id, data).then(function (result) {
                 return result.data;
             });
         }
 
         return {
+            getAll: getAll,
             getById: getById,
+            getByTaskId: getByTaskId,
+            getByProjectId: getByProjectId,
+            getByDiscussionId: getByDiscussionId,
+            getByUserId: getByUserId,
             saveAttachments: saveAttachments,
             updateAttachment: updateAttachment
         };
