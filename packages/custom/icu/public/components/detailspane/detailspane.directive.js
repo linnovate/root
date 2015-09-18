1ee95a3342e5da3e952d5de54bc8163af78e3050
Roman Golovin <danteek@gmail.com>
2015-09-18 16:03:49 +0300

    Change details tabs behavior
    On mouse hover on tabs, their content changes its height
    Description become smaller

--- packages/custom/icu/public/components/detailspane/detailspane.directive.js
+++ packages/custom/icu/public/components/detailspane/detailspane.directive.js
@@ -6,9 +6,28 @@ angular.module('mean.icu.ui.detailspane', [])
 
     }
 
+    function link($scope, $element) {
+        var changeHeight = function(event) {
+            var description = $element.find('.description');
+            var tabContent = $element.find('.tab-content');
+
+            var target = angular.element(event.target);
+
+            var height = target.parents('.tab-content').length ? '30%' : '65%';
+
+            description.height(height);
+            tabContent.height('calc(100% - ' + height + ' - 135px)');
+        };
+
+        var debounced = _.debounce(changeHeight, 100);
+
+        $element.on('mouseover', debounced);
+    }
+
     return {
         restrict: 'A',
         controller: controller,
+        link: link,
         templateUrl: '/icu/components/detailspane/detailspane.html'
     };
 });
