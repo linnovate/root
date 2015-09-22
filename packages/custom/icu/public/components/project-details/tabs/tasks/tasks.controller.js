50d06f6f488501823190751e5050e07d5dae504c
Roman Golovin <danteek@gmail.com>
2015-09-22 12:24:23 +0300

    Enable default sorting in tasks tab
    Refactor tasks tab code

--- packages/custom/icu/public/components/project-details/tabs/tasks/tasks.controller.js
+++ packages/custom/icu/public/components/project-details/tabs/tasks/tasks.controller.js
@@ -3,12 +3,4 @@
 angular.module('mean.icu.ui.projectdetails')
     .controller('ProjectTasksController', function ($scope, entity, context, tasks, $state) {
         $scope.tasks = tasks;
-
-        $scope.manageTasks = function () {
-            $state.go('main.tasks.byentity.tasks', {
-                entity: 'project',
-                id: $scope.project._id,
-                entityId: $scope.project._id
-            });
-        };
     });
---
+++ packages/custom/icu/public/components/tabs/tasks/tasks.directive.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('mean.icu.ui.tabs')
+    .directive('icuTabsTasks', function ($state, $filter) {
+        function controller($scope) {
+            $scope.sorting = {
+                field: 'created',
+                isReverse: false
+            };
+
+            $scope.taskOrder = function(task) {
+                if (task._id && $scope.sorting) {
+                    var parts = $scope.sorting.field.split('.');
+                    var result = task;
+                    for (var i = 0; i < parts.length; i+=1) {
+                        if (result) {
+                            result = result[parts[i]];
+                        } else {
+                            result = undefined;
+                        }
+                    }
+
+                    //HACK: instead of using array of 2 values, this code concatenates
+                    //2 values
+                    //Reason: inconsistency in sorting results between sorting by one param
+                    //and array of params
+                    return result + task.title;
+                }
+            };
+
+            function sort() {
+                var result = $filter('orderBy')($scope.tasks, $scope.taskOrder);
+                Array.prototype.splice.apply($scope.tasks, [0, $scope.tasks.length].concat(result));
+            }
+
+            sort();
+
+            $scope.manageTasks = function () {
+                $state.go('main.tasks.byentity.tasks', {
+                    entity: $scope.entityName,
+                    id: $scope.entity._id,
+                    entityId: $scope.entity._id
+                });
+            };
+        }
+
+        return {
+            restrict: 'A',
+            scope: {
+                tasks: '=',
+                entityName: '=',
+                entity: '='
+            },
+            controller: controller,
+            replace: true,
+            templateUrl: '/icu/components/tabs/tasks/tasks.html'
+        };
+    });
+
