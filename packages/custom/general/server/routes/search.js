60753d573cd36e8fe769daa65685bfb9fc1bee84
oren <oren@linnovate.net>
2015-10-13 17:11:37 +0300

    changed search to use talkToICAPI

--- packages/custom/general/server/routes/search.js
+++ packages/custom/general/server/routes/search.js
@@ -2,25 +2,24 @@
 
 var config = require('meanio').loadConfig(),
   apiUri = config.api.uri,
-  request = require('request');
+    icapi = require('../providers/icapi.js'),
+    request = require('request');
 
 module.exports = function(General, app, auth, database) {
   app.get('/api/search', function(req, res) {
     var index = req.query.index ? ('&index=' + req.query.index) : '';
 
-    var objReq = {
-      uri: apiUri + '/api/search/?' + 'term=' + encodeURI(req.query.term) + index,
+    var options = {
+      cmd: '/api/search/?' + 'term=' + encodeURI(req.query.term) + index,
       method: 'GET',
       gzip: true,
       headers: req.headers
     };
 
-    request(objReq, function(error, response, body) {
-      if (!error && response.statusCode === 200 && response.body.length)
-        return res.json(JSON.parse(response.body));
-      if(response)
-        return res.status(response.statusCode).send(response.body);
-
+    icapi.talkToApi(options, function(data, statusCode){
+      if(statusCode && statusCode != 200)
+        res.status(statusCode);
+      res.send(data);
     });
   });
 };
