cfec8fd2789a979eda42c3a7ef039ed37fb35ddd
Shoshi Baron <shoshi@linnovate.net>
2015-10-13 13:50:19 +0300

    not display modal without tasks

--- packages/custom/icu/public/components/modal-directive/modal.directive.js
+++ packages/custom/icu/public/components/modal-directive/modal.directive.js
@@ -3,16 +3,26 @@ angular.module('mean.icu.ui.modaldeletetasksbyentity', [])
     .directive('icuOpenModal', function ($modal) {
 
         function link(scope, elem, attrs) {
+            console.log(scope,attrs)
+
 
             elem.bind('click', function() {
 
+                if(scope.isTasks) {
+                    buildModal();
+                } else
+                    scope.deleteFn();
+
+            });
+
+            function buildModal() {
                 var modalInstance = $modal.open({
                     animation: true,
                     templateUrl: '/icu/components/modal-directive/modal.html',
                     controller: controller,
                     resolve: {
                         entity: function () {
-                            return scope.entity;
+                            return attrs.icuOpenModal;
                         }
                     }
 
@@ -23,34 +33,36 @@ angular.module('mean.icu.ui.modaldeletetasksbyentity', [])
                 }, function () {
                     console.log('Modal dismissed');
                 });
-            });
-
+            }
         }
 
 
-        function controller($scope, $modalInstance, entity) {
-
-            $scope.entity = {type: entity};
-
-            $scope.ok = function () {
-                if($scope.entity.textDelete && $scope.entity.textDelete == 'DELETE')
-                    $modalInstance.close();
-                else
-                    $scope.cancel();
-
-            };
-
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        }
+        ;
 
         return {
             restrict: 'A',
             scope: {
-                entity: "@",
+                isTasks: "=",
                 deleteFn: '&'
             },
             link: link
         };
     });
+
+function controller($scope, $modalInstance, entity) {
+    console.log(entity,'entity')
+
+    $scope.entity = {type: entity};
+
+    $scope.ok = function () {
+        if($scope.entity.textDelete && $scope.entity.textDelete == 'DELETE')
+            $modalInstance.close();
+        else
+            $scope.cancel();
+
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}
