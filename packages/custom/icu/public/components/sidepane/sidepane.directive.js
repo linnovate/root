da74556d45f0a6b8cef4ae9d787c71f84b62e885
Roman Golovin <danteek@gmail.com>
2015-11-03 18:06:55 +0200

    Fix toggling item in sidepane

--- packages/custom/icu/public/components/sidepane/sidepane.directive.js
+++ packages/custom/icu/public/components/sidepane/sidepane.directive.js
@@ -9,26 +9,40 @@ directive('icuSidepane', function() {
         $scope.discussions = $scope.discussions.data || $scope.discussions;
         $scope.people = $scope.people.data || $scope.discussions;
 
+        $scope.toggleVisibility = function(toggledItem) {
+            var prev = toggledItem.open;
+
+            $scope.items.forEach(function(i) {
+                i.open = false;
+            });
+
+            toggledItem.open = !prev;
+        }
+
         $scope.items = [{
             name: 'tasks',
             icon: '/icu/assets/img/task.png',
             state: 'tasks',
-            display: ['projects', 'discussions', 'people']
+            display: ['projects', 'discussions', 'people'],
+            open: false
         }, {
             name: 'projects',
             icon: '/icu/assets/img/project.png',
             state: 'projects',
-            display: ['discussions', 'people']
+            display: ['discussions', 'people'],
+            open: false
         }, {
             name: 'discussions',
             icon: '/icu/assets/img/meeting.png',
             state: 'discussions',
-            display: ['projects', 'people']
+            display: ['projects', 'people'],
+            open: false
         }, {
             name: 'people',
             icon: '/icu/assets/img/people.png',
             state: 'people',
-            display: ['projects', 'discussions']
+            display: ['projects', 'discussions'],
+            open: false
         }];
 
         $scope.isCurrentState = function(item) {
