bb37265902c948703ed464e74a043125cae896a3
Dvora <dvora@linnovate.net>
2016-03-27 13:15:13 +0300

    users route

--- packages/custom/general/server/routes/users.js
+++ packages/custom/general/server/routes/users.js
@@ -2,38 +2,20 @@
 
 // User routes use users controller
 var config = require('meanio').loadConfig(),
-  apiUri = config.api.uri,
+  // apiUri = config.api.uri,
+  apiUri = "http://localhost:3000",
   request = require('request');
+  var users = require('../../../../core/users/server/controllers/users')
+  var jwt = require('jsonwebtoken');
+  
 
-module.exports = function(MeanUser, app, auth, database) {
+module.exports = function(MeanUser, app, auth, database,passport) {
 
   var UserC = require('../providers/crud.js').User,
     User = new UserC('/api/users');
 
   app.route('/api/signout')
-    .get(function(req, res) {
-      var objReq = {
-        uri: apiUri + '/api/logout',
-        method: 'GET',
-        headers: {
-          connection: req.headers.connection,
-          accept: req.headers.accept,
-          'user-agent': req.headers['user-agent'],
-          authorization: req.headers.authorization,
-          'accept-language': req.headers['accept-language'],
-          cookie: req.headers.cookie,
-          'if-none-match': req.headers['if-none-match']
-        }
-      };
-
-      request(objReq, function(error, response, body) {
-        if (!error && response.statusCode === 200 && response.body.length)
-          return res.redirect('/');
-        if (response)
-          return res.status(response.statusCode).send(response.body);
-
-      });
-    });
+    .get(users.signout);
 
   // Setting up the users api
   app.route('/api/signup')
@@ -55,20 +37,31 @@ module.exports = function(MeanUser, app, auth, database) {
 
   // Setting the local strategy route
   app.route('/api/signin')
-    .post(function(req, res) {
-
-      var objReq = {
-        uri: apiUri + '/api/login',
-        method: 'POST',
-        form: req.body
-      };
-
-      request(objReq, function(error, response, body) {
-        if (!error && response.statusCode === 200 && response.body.length)
-          return res.json(JSON.parse(response.body));
-        if(response)
-          return res.status(response.statusCode).send(response.body);
-
-      });
+  	.post(passport.authenticate('local', {
+      failureFlash: false
+    }), function(req, res) {      
+      var payload = req.user;
+      payload.redirect = req.body.redirect;
+      var escaped = JSON.stringify(payload);      
+      escaped = encodeURI(escaped);
+      // We are sending the payload inside the token
+      var token = jwt.sign(escaped, config.secret, { expiresInMinutes: 60*5 });
+      res.json({ token: token });
     });
+    // .post(function(req, res) {
+
+    //   var objReq = {
+    //     uri: apiUri + '/api/login',
+    //     method: 'POST',
+    //     form: req.body
+    //   };
+
+    //   request(objReq, function(error, response, body) {
+    //     if (!error && response.statusCode === 200 && response.body.length)
+    //       return res.json(JSON.parse(response.body));
+    //     if(response)
+    //       return res.status(response.statusCode).send(response.body);
+
+    //   });
+    // });
 };
