c414bf7f9ae5691e93ca69640020bca2118adb05
ohadetsion <etsionohad@gmail.com>
2016-03-27 23:15:31 -0700

    elastic

---
+++ packages/custom/elasticsearch/app.js
@@ -0,0 +1,103 @@
+'use strict';
+
+var mean = require('meanio');
+var esConfig = mean.loadConfig().elasticsearch;
+var Module = mean.Module;
+var elasticsearch = require('elasticsearch');
+
+var Elasticsearch = new Module('elasticsearch');
+
+Elasticsearch.register(function(app, auth, database) {
+
+  Elasticsearch.routes(app, auth, database);
+
+  Elasticsearch.connect = function() {
+      delete Elasticsearch.client;
+
+      Elasticsearch.settings(function(err, config) {
+
+          if (err) {
+              return console.log('error retrieving Elasticsearch settings');
+          }
+
+          var host = esConfig.host;
+          var port = esConfig.port;
+          var log = esConfig.log ? esConfig.log : 'trace';
+
+          Elasticsearch.settings({host:host,port:port,log:log});
+
+          Elasticsearch.client = new elasticsearch.Client({
+              host: host + ':' + port,
+              log: log,
+                    keepAlive: false,
+                    sniffOnConnectionFault:true,
+                    maxRetries:50
+          });
+      });
+  };
+
+  Elasticsearch.connect();
+
+
+  Elasticsearch.ping = function(callback) {
+    Elasticsearch.client.ping({
+      requestTimeout: 1000000000,
+      hello: 'elasticsearch!'
+    }, function(error) {
+      if (error) {
+        callback(true, 'cluster down');
+      } else {
+        callback(null, 'OK');
+      }
+    });
+  };
+
+    Elasticsearch.index = function(options, callback) {
+        Elasticsearch.client.index(options, function(error, response) {
+            if (error) {
+                callback(true, error);
+            } else {
+                callback(null, response);
+            }
+        });
+    };
+
+
+  Elasticsearch.create = function(options, callback) {
+    Elasticsearch.client.create(options, function(error, response) {
+      if (error) {
+        callback(true, error);
+      } else {
+        callback(null, response);
+      }
+    });
+  };
+
+    Elasticsearch.delete = function(options, callback) {
+        Elasticsearch.client.delete(options, function(error, response) {
+            if (error) {
+                callback(true, error);
+            } else {
+                callback(null, response);
+            }
+        });
+    };
+
+  Elasticsearch.search = function(options, callback) {
+    Elasticsearch.client.search(options, function(error, response) {
+        console.log("error===========================");
+        console.log(error);
+        console.log("response===========================");
+        console.log(response);
+      if (error) {
+        callback(true, error);
+      } else {
+        callback(null, response);
+      }
+    });
+  };
+
+  return Elasticsearch;
+});
+
+mean.elasticsearch = Elasticsearch;
---
+++ packages/custom/elasticsearch/public/routes/elasticsearch.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mean.elasticsearch').config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider.state('elasticsearch status', {
+      url: '/elasticsearch/status',
+      templateUrl: 'elasticsearch/views/status.html'
+    });
+  }
+]);
