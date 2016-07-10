584ebd1168fe78ad0e28eb71f2a2e1460c29872a
Sara Reisner <sarar@linnovate.net>
2016-07-10 15:22:48 +0300

    css fixes

--- packages/custom/hi/server/controllers/hi.js
+++ packages/custom/hi/server/controllers/hi.js
@@ -54,6 +54,7 @@ exports.talkToHi = function(options, callback) {
 
 exports.talkToRocketChat = function(options, callback) {
 	console.log('================================================talkToRocketChat======================================')
+	
 	// Logon with REST API
 	// var objReq = {
  //        uri: 'https://hirc.herokuapp.com/api/login',
