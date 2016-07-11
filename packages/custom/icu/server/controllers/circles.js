31f34c748686e674660bf3007209c299d0488b52
Rivka Thur <rivkat@linnovate.net>
2016-07-11 13:39:32 +0300

    request querystring

--- packages/custom/icu/server/controllers/circles.js
+++ packages/custom/icu/server/controllers/circles.js
@@ -3,7 +3,8 @@
 var config = require('meanio').loadConfig(),
   actionSettings = require(process.cwd() + '/config/actionSettings') || {};
 
-var request = require('request');
+var request = require('request'),
+  qs = require('querystring');
 
 var redis = require('redis'),
   client = redis.createClient();
@@ -14,7 +15,8 @@ client.on('error', function(err) {
 
 
 var getCircles = exports.getCircles = function(type, user, callback) {
-  var api = '/api/v1/circles/' + type + '?user=' + user;
+  var query = qs.stringify({user: user})
+  var api = '/api/v1/circles/' + type + '?' + query;
   client.get(api, function(err, reply) {
     if (reply) return callback(null, reply);
     request(config.circles.uri + api, function(error, response, body) {
