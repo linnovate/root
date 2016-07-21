b520171c16f8b331291d18d496d00252fdd15806
Rivka Thur <rivkat@linnovate.net>
2016-07-21 14:16:58 +0300

    save circles in mongo

--- packages/custom/icu/server/controllers/circles.js
+++ packages/custom/icu/server/controllers/circles.js
@@ -1,29 +1,36 @@
 'use strict';
 
+require('../models/circle');
+var mongoose = require('mongoose'),
+  Circle = mongoose.model('Circle');
+
 var config = require('meanio').loadConfig(),
   actionSettings = require(process.cwd() + '/config/actionSettings') || {};
 
 var request = require('request'),
   qs = require('querystring');
 
-var redis = require('redis'),
-  client = redis.createClient();
-
-client.on('error', function(err) {
-  console.log('Error ' + err);
-});
-
-
 var getCircles = exports.getCircles = function(type, user, callback) {
   var query = qs.stringify({
     user: user
-  })
+  });
   var api = '/api/v1/circles/' + type + '?' + query;
-  client.get(api, function(err, reply) {
-    if (reply) return callback(null, reply);
+  Circle.findOne({
+    key: api
+  }, function(err, circle) {
+    var date = new Date();
+    if (circle && circle.created > new Date(date.getTime() - 60000 * actionSettings.cacheTime)) return callback(null, circle.value);
     request(config.circles.uri + api, function(error, response, body) {
       if (error || response.statusCode !== 200) return callback(error || response.statusCode);
-      client.setex(api, actionSettings.cacheTime, body, redis.print);
+      Circle.findOneAndUpdate({
+        key: api
+      }, {
+        key: api,
+        value: body,
+        created: date
+      }, {
+        upsert: true
+      }, function() {})
       return callback(null, body);
     });
   });
