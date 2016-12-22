562cb7807f51ec05818d359e5c8ef6f6f1770f5d
Rivka Thur <rivkat@linnovate.net>
2016-12-22 11:04:45 +0200

    circles strong types

--- config/circleSettings.js
+++ config/circleSettings.js
@@ -26,15 +26,25 @@ module.exports = {
 		personal: {
 			requiredAllowed: false,
 			max: 50,
-			// watchers: true,
-			stronger: ['watchers'],
+			stronger: [{
+				name: 'watchers',
+				type: 'Array'
+			}, {
+				name: 'assign',
+				type: 'String'
+			}],
 			location: 'footer'
 		},
 		corporate: {
 			requiredAllowed: false,
 			max: 50,
-			// watchers: true,
-			stronger: ['watchers'],
+			stronger: [{
+				name: 'watchers',
+				type: 'Array'
+			}, {
+				name: 'assign',
+				type: 'String'
+			}],
 			location: 'footer'
 		}
 	},
