594b27f3f70cf0def9312143bf69a75bc133782c
Dvora Gadasi <dvorad@linnovate.net>
2017-01-23 10:09:14 +0200

    fix resolution

--- e2eTesting/protractor-browserstack-e2e/conf/single.conf.js
+++ e2eTesting/protractor-browserstack-e2e/conf/single.conf.js
@@ -1,9 +1,9 @@
 exports.config = {
   'specs': ['../specs/single.js'],
   'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
-//   'onPrepare: function() {
-//   browser.manage().window().setSize(1600, 1000);
-// }
+  'onPrepare': function() {
+  browser.driver.manage().window().maximize();
+},
 
   'capabilities': {
     'browserstack.user': process.env.dvoragadasi1 || 'dvoragadasi1',
@@ -11,12 +11,13 @@ exports.config = {
     'build': 'protractor-browserstack',
     'name': 'single_test',
     'browserName': 'chrome',
-    'resolution': '1024x768',
+    'resolution': '1920x1080',
     'browserstack.debug': 'true',
      'browser_version': '52.0',
      'os': 'Windows',
      'os_version': '10'
   },
+  framework: 'jasmine2',
   'jasmineNodeOpts': {
     'showColors': true,
     // 'defaultTimeoutInterval': 50000
