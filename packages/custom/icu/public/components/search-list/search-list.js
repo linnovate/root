1f0bf250dcc54e79953ffbabba46338bee8c05a4
Barak Steindl <steindl.barak@gmail.com>
2017-04-25 05:07:44 -0700

    Unauthorized search problem fixed

--- packages/custom/icu/public/components/search-list/search-list.js
+++ packages/custom/icu/public/components/search-list/search-list.js
@@ -1,7 +1,20 @@
 'use strict';
 
 angular.module('mean.icu.ui.searchlist')
-.controller('SearchListController', function ($scope, results, term) {
+.controller('SearchListController', function ($scope, results, term, UsersService) {
     $scope.results = results;
-    $scope.term = term;
-});
+    UsersService.getMe().then(function(me){
+    	var id = me._id;
+    	var finalResults = [];
+    	for(var i=0;i<results.length;i++){
+    		var task = results[i];
+    		if(task.creator == id || task.assign == id || $.inArray(id, task.watchers)!=-1){
+    			finalResults.push(task);
+    		}
+    	}
+    	$scope.term = term;
+    	$scope.results = finalResults;
+    	});
+    });
+   
+
