1b0e8e8b09835eb7801047d70f3c4953edd6fb3f
Dvora B <dvora@linnovate.net>
2017-10-03 14:33:34 +0300

    remove changes

--- config/env/development.js
+++ config/env/development.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   //hostname: 'http://192.168.245.152:3000',
 //  host: 'http://root.demo.linnovate.net:3008',
-  host: 'http://127.0.0.1',
+  host: 'http://localhost',
   facebook: {
     clientID: 'DEFAULT_APP_ID',
     clientSecret: 'APP_SECRET',
