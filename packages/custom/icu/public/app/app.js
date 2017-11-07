defdf15ddc017e778fb88ad94aac7ef0c30d6e98
sraya <sraya305@gmail.com>
2017-11-07 13:43:43 +0200

    fixed

--- packages/custom/icu/public/app/app.js
+++ packages/custom/icu/public/app/app.js
@@ -1442,6 +1442,17 @@ angular.module('mean.icu').config([
             .state('main.search.folder.documents', getDetailsTabState('folder', 'documents'))
             .state('main.search.folder.tasks', getDetailsTabState('folder', 'tasks'))
 
+            .state('main.search.officedocument', getOfficeDocumentDetailsState('/officeDocument'))
+            .state('main.search.officedocument.activities', getDetailsTabState('officeDocument', 'activities'))
+            .state('main.search.officedocument.activities.modal', getDetailspaneModal())
+            .state('main.search.officedocument.documents', getDetailsTabState('officeDocument', 'documents'))
+
+            // .state('main.officeDocuments.byentity.details', getOfficeDocumentDetailsState())
+            // .state('main.officeDocuments.byentity.details.activities', getDetailsTabState('officeDocument', 'activities'))
+            // .state('main.officeDocuments.byentity.details.activities.modal', getDetailspaneModal())
+            // .state('main.officeDocuments.byentity.details.documents', getDetailsTabState('officeDocument', 'documents'))
+            // .state('main.officeDocuments.byentity.details.tasks', getDetailsTabState('officeDocument', 'tasks'))
+
             .state('main.search.attachment', getAttachmentDetailsState('/attachment'))
             .state('main.search.attachment.versions', getAttachmentDetailsTabState())
 
