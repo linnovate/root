fe743d92abab1cf97e22517b3a2ee14f495308dc
Orit Persik <orit@linnovate.net>
2017-12-11 15:05:20 +0200

    angular module

--- packages/custom/icu/public/components/admin/adminSettings.js
+++ packages/custom/icu/public/components/admin/adminSettings.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//angular.module('mean.icu.ui.webhook', []);
+
 angular.module('mean.icu.ui.admin', []).controller('adminSettingsController', 
     function($scope, me) {
         $scope.me = me;
