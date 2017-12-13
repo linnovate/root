3648f08de07e5b2262c70e404027cad99f76e4a0
Dvora B <dvora@linnovate.net>
2017-12-13 14:58:41 +0200

    search results

--- packages/custom/icu/public/components/search-list/search-list.js
+++ packages/custom/icu/public/components/search-list/search-list.js
@@ -3,7 +3,26 @@
 angular.module('mean.icu.ui.searchlist')
 .controller('SearchListController', function ($scope, results, term, UsersService) {
 	$scope.results = results;
-	
+
+	for (var i = 0; i< $scope.results.length; i++) {
+		if ($scope.results[i].project)
+			$scope.results[i].projectObj = $scope.$parent.projects.find(function(e) { 
+				return e._id == $scope.results[i].project
+			})
+		/*if ($scope.results[i].discussions && $scope.results[i].discussions.length)
+			$scope.results[i].discussionObj = $scope.$parent.discussions.find(function(e) { 
+				return e._id == $scope.results[i].discussions[0]
+			})
+		if ($scope.results[i].folder)
+			$scope.results[i].folderObj = $scope.$parent.folders.find(function(e) { 
+				return e._id == $scope.results[i].folder
+			})*/
+		if ($scope.results[i].office)
+			$scope.results[i].officeObj = $scope.$parent.offices.find(function(e) { 
+				return e._id == $scope.results[i].office
+			})
+	}
+
 	$scope.inObjArray = function(id,array){
 		array.forEach(function(w){
 			if(w._id && w._id==id){
