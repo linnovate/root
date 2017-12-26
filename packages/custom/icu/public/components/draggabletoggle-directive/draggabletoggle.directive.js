e44a891c8ae2364ccfe0c7dc83605b08545c5860
nevos <nevonat@gmail.com>
2017-12-26 16:03:38 +0200

    test draggable toggle

---
+++ packages/custom/icu/public/components/draggabletoggle-directive/draggabletoggle.directive.js
@@ -0,0 +1,91 @@
+'use strict';
+angular.module('mean.icu.ui.draggabletoggle', [])
+    .directive('draggableToggle', function($document) {
+        console.log("draggableToggle") ;
+  return {
+    restrict: 'A',
+    scope: {
+      dragOptions: '=ngDraggable'
+    },
+    link: function(scope, elem, attr) {
+      var startX, startY, x = 0, y = 0,
+          start, stop, drag, container;
+
+      var width  = elem[0].offsetWidth,
+          height = elem[0].offsetHeight;
+
+      // Obtain drag options
+      if (scope.dragOptions) {
+        start  = scope.dragOptions.start;
+        drag   = scope.dragOptions.drag;
+        stop   = scope.dragOptions.stop;
+        var id = scope.dragOptions.container;
+        if (id) {
+            container = document.getElementById(id).getBoundingClientRect();
+        }
+      }
+
+      // Bind mousedown event
+      elem.on('mousedown', function(e) {
+        e.preventDefault();
+        startX = e.clientX - elem[0].offsetLeft;
+        startY = e.clientY - elem[0].offsetTop;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+        if (start) start(e);
+      });
+
+      // Handle drag event
+      function mousemove(e) {
+        y = e.clientY - startY;
+        x = e.clientX - startX;
+        setPosition();
+        if (drag) drag(e);
+      }
+
+      // Unbind drag events
+      function mouseup(e) {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+        if (stop) stop(e);
+      }
+
+      // Move element, within container if provided
+      function setPosition() {
+        if (container) {
+          if (x < container.left) {
+            x = container.left;
+          } else if (x > container.right - width) {
+            x = container.right - width;
+          }
+          if (y < container.top) {
+            y = container.top;
+          } else if (y > container.bottom - height) {
+            y = container.bottom - height;
+          }
+        }
+
+        elem.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+    }
+  }
+
+}).controller('testCtrl', function($scope) { 
+    console.log("testCtrl") ;
+    $scope.dragOptions = {
+        start: function(e) {
+          console.log("STARTING");
+        },
+        drag: function(e) {
+          console.log("DRAGGING");
+        },
+        stop: function(e) {
+          console.log("STOPPING");
+        },
+        container: 'container'
+    }
+
+});
