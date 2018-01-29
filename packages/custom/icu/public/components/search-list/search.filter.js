5f51bd067afc8cdf2f80bd2b9ad4c44ae9c0c66b
nevos <nevonat@gmail.com>
2018-01-29 12:50:35 +0200

    code fix

--- packages/custom/icu/public/components/search-list/search.filter.js
+++ packages/custom/icu/public/components/search-list/search.filter.js
@@ -7,9 +7,8 @@ angular.module('mean.icu.ui.searchlistfilter', [])
                 // console.log("filter date", SearchService.filteringByUpdated) ;
                 // console.log("entity update", e.updated) ;
                 let res =  new Date(e.updated) > new Date(SearchService.filteringByUpdated); // true if time1 is later
-                console.log(res) ;
+//                console.log(res) ;
                 return res ? e.id : -1 ;
-//                return e.id
             });
             
             var out = [];
@@ -19,21 +18,7 @@ angular.module('mean.icu.ui.searchlistfilter', [])
                 }
             }
             return out;
-        }  
-
-        // let filteringByUpdated = SearchService.filteringByUpdated ;
-        // let filteringResults = [] ;
-        // if (!filteringByUpdated) {
-        //     console.log("filter date not set") ;
-        //     return SearchService.filteringResults ;
-        // }
-
-        // filteringResults = SearchService.filteringResults.filter(function(item) {
-        // console.log(item) ;
-        // console.log(filteringByUpdated) ;
-        // return true ;
-    });
-
+        }
     return out = filteringResults ;
     })
 
