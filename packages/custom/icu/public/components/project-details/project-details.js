f3d7fe39f1c0c02bd1e0df8d5f3cde885c6b477e
Daniel Rosenkranz <nevonat@gmail.com>
2018-03-26 09:43:42 +0300

    adding project due date support

--- packages/custom/icu/public/components/project-details/project-details.js
+++ packages/custom/icu/public/components/project-details/project-details.js
@@ -208,6 +208,7 @@ angular.module('mean.icu.ui.projectdetails', [])
                 project.discussion = context.entityId;
             }
 
+            
             ProjectsService.updateDue(project, backupEntity).then(function(result) {
                 backupEntity = JSON.parse(JSON.stringify($scope.project));
                 ActivitiesService.data.push(result);
