d56ac90890793deae07bdbcb7227417631207c78
Daniel Rosenkranz <nevonat@gmail.com>
2018-04-15 15:59:50 +0300

    add rootscope to officeDocs

--- packages/custom/icu/public/components/officeDocument-details/officeDocument-details.js
+++ packages/custom/icu/public/components/officeDocument-details/officeDocument-details.js
@@ -3,6 +3,7 @@
 angular.module('mean.icu.ui.officeDocumentdetails', [])
     .controller('OfficeDocumentDetailsController', function ($scope,
                                                       entity,
+                                                      $rootScope,
                                                       tasks,
                                                       people,
                                                       officeDocuments,
