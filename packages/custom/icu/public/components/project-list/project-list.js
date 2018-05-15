9e31baacf4bfcaeb85dda183f9372cca02985816
manmorc <makeiTEasysoft@mail.ru>
2018-05-15 15:10:37 +0300

    added scrolling to middlepane, and to detailspane, refactoring
    (cherry picked from commit 92a42ef)
    
    (cherry picked from commit 5cc14b8)

--- packages/custom/icu/public/components/project-list/project-list.js
+++ packages/custom/icu/public/components/project-list/project-list.js
@@ -1,19 +1,62 @@
 'use strict';
 
-function ProjectListController($scope, $state, projects, ProjectsService, context, $stateParams, EntityService) {
+function ProjectListController($scope,
+                               $window,
+                               $state,
+                               projects,
+                               ProjectsService,
+                               context,
+                               $filter,
+                               $stateParams,
+                               EntityService) {
 
-    $scope.items = projects.data || projects;
+    $scope.projects = projects.data || projects;
     $scope.loadNext = projects.next;
     $scope.loadPrev = projects.prev;
-    
+
     $scope.entityName = 'projects';
     $scope.entityRowTpl = '/icu/components/project-list/project-row.html';
-    
-    var creatingStatuses = {
-        NotCreated: 0,
-        Creating: 1,
-        Created: 2
+
+
+    ProjectsService.getById($state.params.entityId).then(result=>{
+        $scope.parentName = result.title;
+    });
+
+    $scope.print = function() {
+        $window.print()
+    };
+
+    $scope.starred = $stateParams.starred;
+
+    // activeToggle
+    $scope.activeToggleList = EntityService.activeToggleList;
+    $scope.activeToggle = {
+            field: !EntityService.isActiveStatusAvailable() ? 'all' : $stateParams.activeToggle || 'active',
+            disabled: !EntityService.isActiveStatusAvailable()
+    };
+    /*---*/
+
+    if ($scope.projects.length > 0 && !$scope.projects[$scope.projects.length - 1].id) {
+        $scope.projects = [$scope.projects[0]];
     }
+
+    $scope.goToParent = function() {
+        $state.go('main.projects.'+$scope.parentState+'.details',{entity:$scope.parentEntity,entityId:$scope.parentEntityId,id:$scope.parentId})
+    };
+
+    $scope.isCurrentState = function (id) {
+        return $state.current.name.indexOf('main.projects.byentity') === 0 &&
+            $state.current.name.indexOf('details') === -1;
+    };
+
+    $scope.changeOrder = function () {
+        if($scope.sorting.field != "custom"){
+            $scope.sorting.isReverse = !$scope.sorting.isReverse;
+        }
+
+        /*Made By OHAD - Needed for reversing sort*/
+        $state.go($state.current.name, { sort: $scope.sorting.field });
+    };
 
     $scope.update = function(item) {
         return ProjectsService.update(item.title);
@@ -34,23 +77,75 @@ function ProjectListController($scope, $state, projects, ProjectsService, contex
             ProjectsService.data.push(result);
             return result;
         });
+    };
+
+    $scope.sorting = {
+        field: $stateParams.sort || 'created',
+        isReverse: false
+    };
+
+    $scope.sortingList = [
+        {
+            title: 'title',
+            value: 'title'
+        }, {
+            title: 'status',
+            value: 'status'
+        }, {
+            title: 'created',
+            value: 'created'
+        }
+    ];
+
+     if(context.entityName != "all"){
+        $scope.sortingList.push({
+            title: 'custom',
+            value: 'custom'
+        });
     }
 
-//     $scope.search = function(item) {
-//         return ProjectsService.search(term).then(function(searchResults) {
-//             _(searchResults).each(function(sr) {
-//                 var alreadyAdded = _($scope.items).any(function(p) {
-//                     return p._id === sr._id;
-//                 });
+    function navigateToDetails(project) {
+        if(!project) return ;
 
-//                 if (!alreadyAdded) {
-//                     return $scope.searchResults.push(sr);
-//                 }
-//             });
-//             $scope.selectedSuggestion = 0;
-//         });
-//     }
+        $scope.detailsState = context.entityName === 'all' ?
+            'main.projects.all.details' : 'main.projects.byentity.details';
 
+        $state.go($scope.detailsState, {
+            id: project._id,
+            entity: $scope.currentContext.entityName,
+            entityId: $scope.currentContext.entityId,
+        });
+    }
+
+    $scope.toggleStarred = function () {
+        $state.go($state.current.name, { starred: !$stateParams.starred });
+    };
+
+    $scope.filterActive = function () {
+        EntityService.activeStatusFilterValue = $scope.activeToggle.field ;
+        $state.go($state.current.name, { activeToggle: $scope.activeToggle.field });
+    };
+
+    let possibleNavigate = $scope.projects.filter(function(t) {
+        return t.recycled == null ;
+    })
+
+    if (possibleNavigate.length) {
+        if ($state.current.name === 'main.projects.all' ||
+            $state.current.name === 'main.projects.byentity') {
+            navigateToDetails(possibleNavigate[0]);
+        }
+    }
+    else {
+        if ($state.current.name === 'main.projects.all') {
+            return;
+        }
+        if (
+            $state.current.name !== 'main.projects.byentity.activities' &&
+            $state.current.name !== 'main.projects.byentity.details.activities') {
+            $state.go('.activities');
+        }
+    }
     $scope.loadMore = function(start, LIMIT, sort) {
         if (!$scope.isLoading && $scope.loadNext) {
             $scope.isLoading = true;
