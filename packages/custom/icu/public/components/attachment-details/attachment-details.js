fcb473fdca658ace1f7c7f0d104e7f414e826d90
aron <astein@linnovate.net>
2018-05-27 19:31:48 +0300

    fix attachment tpl

--- packages/custom/icu/public/components/attachment-details/attachment-details.js
+++ packages/custom/icu/public/components/attachment-details/attachment-details.js
@@ -1,83 +1,69 @@
 'use strict';
 
-angular.module('mean.icu.ui.attachmentdetails', [])
-    .controller('AttachmentDetailsController', function ($scope,
-                                                        $http,
-                                                         entity,
-                                                         tasks,
-                                                         context,
-                                                         $state,
-                                                         AttachmentsService,
-                                                         DocumentsService,
-                                                         UsersService,
-                                                         EntityService,
-                                                         $stateParams) {
-        $scope.update = entity || context.entity;
-        $scope.tasks = tasks;
-        $scope.shouldAutofocus = !$stateParams.nameFocused;
-        $scope.attachment = entity ;
-        let attachment = $scope.attachment ;         
-        
-        $scope.attName =  attachment.name ;
-        $scope.attCreated =  attachment.created ; 
-        $scope.attSize =  attachment.size ;
-        $scope.attCreator = null ; 
-        $scope.attUser ; 
-        UsersService.getById(attachment.creator).then(user => $scope.attUser = user) ;
-        $scope.attPath =  attachment.path ;  
-        $scope.attPrint =  attachment.size ;
-        $scope.attType =  attachment.attachmentType;
-        $scope.attLinkToEntityName ;
-        EntityService.getByEntityId(attachment.entity,attachment.entityId).then(res => $scope.attLinkToEntityName = res) ;
+angular.module('mean.icu.ui.attachmentdetails', []).controller('AttachmentDetailsController', AttachmentDetailsController);
 
-        
+function AttachmentDetailsController($scope, $http, entity, tasks, context, $state, AttachmentsService, DocumentsService, UsersService, EntityService, $stateParams) {
+ 
+  // ==================================================== init ==================================================== //
 
-        $scope.attLinkToEntity = '/' + attachment.entity + 's/all/' + attachment.entityId + '/documents';
+  if ($scope.update && $state.current.name === 'main.search.update') {
+    $state.go('.versions');
+  }
 
+  $scope.update = entity || context.entity;
+  $scope.item = entity;
 
-        $scope.$watchGroup(['update.description', 'update.title'], function (nVal, oVal) {
-            if (nVal !== oVal && oVal) {
-                $scope.delayedUpdate($scope.update);
-            }
-        });
+  $scope.tasks = tasks;
 
-        $scope.options = {
-            theme: 'bootstrap',
-            buttons: ['bold', 'italic', 'underline', 'anchor', 'quote', 'orderedlist', 'unorderedlist']
-        };
+  let attachment = $scope.item;
 
-        function navigateToDetails(update) {
-            $scope.detailsState = 'main.search.update';
+  $scope.attName = attachment.name;
+  $scope.attCreated = attachment.created;
+  $scope.attSize = attachment.size;
+  $scope.attCreator = null;
+  $scope.attUser;
+  UsersService.getById(attachment.creator).then(user=>$scope.attUser = user);
+  $scope.attPath = attachment.path;
+  $scope.attPrint = attachment.size;
+  $scope.attType = attachment.attachmentType;
+  $scope.attLinkToEntityName;
+  EntityService.getByEntityId(attachment.entity, attachment.entityId).then(res=>$scope.attLinkToEntityName = res);
 
-            $state.go($scope.detailsState, {
-                id: update._id,
-                entity: $scope.currentContext.entityName,
-                entityId: $scope.currentContext.entityId,
-                starred: $stateParams.starred
-            }, {reload: true});
-        }
+  $scope.attLinkToEntity = '/' + attachment.entity + 's/all/' + attachment.entityId + '/documents';
 
-        $scope.viewAttachment = function (document1) { 
-                        return(document1.path + '?view=true') ;      
-        }
+  $scope.$watchGroup(['item.description', 'item.title'], function(nVal, oVal) {
+    if (nVal !== oVal && oVal) {
+      $scope.delayedUpdate($scope.update);
+    }
+  });
 
-        $scope.previewTab = function(document) {
-            AttachmentsService.previewTab(document) ;
-        }       
+  $scope.viewAttachment = function(document1) {
+    return (document1.path + '?view=true');
+  }
 
-        $scope.deleteUpdate = function (update) {
-            DocumentsService.remove(update._id).then(function () {
-                $state.reload();
-            });
-        };
+  $scope.previewTab = function(document) {
+    AttachmentsService.previewTab(document);
+  }
 
-        $scope.update = function (update) {
-            DocumentsService.update(update);
-        };
+  $scope.update = function(update) {
+    DocumentsService.update(update);
+  }
 
-        $scope.delayedUpdate = _.debounce($scope.update, 500);
+  $scope.delayedUpdate = _.debounce($scope.update, 500);
 
-        if ($scope.update && $state.current.name === 'main.search.update') {
-            $state.go('.versions');
-        }
-    });
+  // ==================================================== Menu events ==================================================== //
+
+//   $scope.deleteUpdate = function(update) {
+//     DocumentsService.remove(update._id).then(function() {
+//       $state.reload();
+//     });
+//   }
+  
+//   $scope.menuItems = [{
+//     label: 'deleteAttachment',
+//     icon: 'times-circle',
+//     display: !$scope.item.hasOwnProperty('recycled'),
+//     action: $scope.recycle,
+//   }];
+
+}
