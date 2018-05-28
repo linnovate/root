4c47787ec16f8688d4e697c0d86819fe84d39273
Avraham Ostreicher <avraham@linnovate.net>
2018-05-28 18:43:49 +0300

    Improve gulp
      * Make `paths` more focused - now `gulp` is much faster
      * Remove unnecessary code
      * Move mongoExpress task to gulp/utils.js
      * Update tasks: 'clean', 'development'
      * Update package.json

--- gulp/utils.js
+++ gulp/utils.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var del = require('del');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var plugins = gulpLoadPlugins;
-
-gulp.task('help', plugins.taskListing);
 
 gulp.task('clean', function (cb) {
-  return del(['bower_components/build', 'dist'], cb);
+  return del([
+    'dist',
+    'packages/custom/icu/public/assets/css/styles.css' // produced by task 'sass'
+  ], cb);
+});
+
+gulp.task('mongoExpress', function () {
+  console.log("Staring mongoExpress... http://localhost:3017/")
+
+  var child_process = require('child_process');
+  process.chdir('node_modules/mongo-express');
+
+  var server = child_process.spawn('node', ['app.js','-a', '-U', 'mongodb://localhost:27017', '--port', '3017'], {
+    stdio: [0,'pipe',0]
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
 
-var env = process.env.NODE_ENV || 'development';
-/*
-var defaultTasks = ['clean', 'jshint', 'csslint','serve','watch']; // initialize with development settings
-if (env === 'production') { var defaultTasks = ['clean', 'cssmin', 'uglify', 'serve', 'watch'];}
-if (env === 'test')       { var defaultTasks = ['env:test', 'karma:unit', 'mochaTest'];}
-*/
-// read gulp directory contents for the tasks...
 require('require-dir')('./gulp');
-console.log('Invoking gulp -',env);
-gulp.task('default', ['clean'], function (defaultTasks) {
-  // run with paramater
-  gulp.start(env);
+
+gulp.task('default', ['development']);
+
+gulp.task('development', function(callback) {
+  process.env.NODE_ENV = 'development';
+  runSequence(
+    'clean',
+    'watch',
+    'sass',
+    ['dist:css', 'dist:public', 'dist:bower', 'babel'],
+    'devServe',
+    callback
+  );
 });
