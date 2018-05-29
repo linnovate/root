99283e4e553b75359ea269dbf5077429b03f48d6
Orit Persik <orit@linnovate.net>
2018-05-29 10:33:01 +0300

    dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM node:4
+FROM node:8
 #test1
+RUN npm install -g npm@latest
 RUN npm install -g bower gulp
 RUN npm install -g nodemon@1.11.0
 RUN mkdir -p /usr/src/app
@@ -9,7 +10,6 @@ RUN chown -R node:node /usr/src/app
 USER node:node
 RUN echo '{}' > $HOME/.mean
 RUN npm install
-RUN node tools/scripts/postinstall.js
 RUN bower install
 ENV PORT 3100
 ENV MONGODB_URI mongodb://root-db/icu
--- packages/custom/icu/server/routes/icu.js
+++ packages/custom/icu/server/routes/icu.js
@@ -67,7 +67,7 @@ module.exports = function(Icu, app) {
   });
   //END update mapping - OHAD
 
-  app.route('/api/:entity(officeDocsFiles|tasks|discussions|projects|users|circles|files|attachments|updates|templates|myTasksStatistics|event-drops|offices|folders|officeDocuments|officeTemplates|templateDocs)*').all(circles.acl());
+  app.route('/api/:entity(officeDocsFiles|tasks|discussions|projects|users|circles|files|attachments|updates|templates|myTasksStatistics|event-drops|offices|folders|officeDocuments|officeTemplates|templateDocs|new)*').all(circles.acl());
 
   app.use('/api/files', attachments.getByPath, error, express.static(config.attachmentDir));
   app.use('/api/Excelfiles', express.static(config.attachmentDir));
