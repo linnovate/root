5add9ad007283702226bdf8c13c21dee60610e35
manmorc <makeiTEasysoft@mail.ru>
2018-07-10 19:46:40 +0300

    added excel button in task list

--- packages/custom/icu/public/components/entity-list/entity-list.js
+++ packages/custom/icu/public/components/entity-list/entity-list.js
@@ -5,7 +5,7 @@
  * @example <div acme-order-calendar-range></div>
  */
 
-function EntityListController($scope, $window, $state, context, $filter, $stateParams, EntityService, dragularService, $element, $interval, $uiViewScroll, $timeout, LayoutService, UsersService, PermissionsService) {
+function EntityListController($scope, $window, $state, context, $filter, $stateParams, EntityService, dragularService, $element, $interval, $uiViewScroll, $timeout, LayoutService, UsersService, TasksService,  PermissionsService) {
 
     // ============================================================= //
     // ========================= navigate ========================== //
@@ -144,6 +144,16 @@ function EntityListController($scope, $window, $state, context, $filter, $stateP
 
     $scope.context = context;
     $scope.isLoading = true;
+
+    $scope.taskList = $state.current.name.indexOf('task') !== -1;
+    $scope.excel = function() {
+        TasksService.excel();
+        var me;
+        UsersService.getMe().then(function(me1) {
+            me = me1;
+            window.open(window.origin + '/api/Excelfiles/notes/' + me.id + 'Tasks.xlsx');
+        });
+    }
 
     $scope.onClickRow = function($event, item) {
         if ($scope.displayOnly) {
