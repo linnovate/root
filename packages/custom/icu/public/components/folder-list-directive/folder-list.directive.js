9ce55dc37cfff02e1a694ba73d03f20273ae76ce
manmorc <makeiTEasysoft@mail.ru>
2018-07-12 14:51:48 +0300

    removed unused functions

--- packages/custom/icu/public/components/folder-list-directive/folder-list.directive.js
+++ packages/custom/icu/public/components/folder-list-directive/folder-list.directive.js
@@ -12,97 +12,11 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
             $scope.currentFolderId = function (id) {
                 $scope.folderId = id;
             };
-            if(($scope.order != null) && ($scope.order.field == "custom")){
-                var timer,
-                    container = $('.containerVertical'),
-                    scroll = $('.list-table'),
-                    box = $('middlepane-container'),
-                    topBar = $('.filters'),
-                    buttomBar = $('.bottomBar');
 
-                dragularService.cleanEnviroment();
-
-                dragularService(container, {
-                    scope: $scope,
-                    boundingBox: box,
-                    lockY: true,
-                    moves: function (el, container, handle) {
-                        return handle.className === 'move';
-                    }
-                });
-
-                $scope.$on('dragulardrag', function (e, el) {
-                    e.stopPropagation();
-                    $('tr').removeClass('active')
-                    el.className = 'active';
-                });
-
-                $scope.$on('dragulardrop', function (e, el, targetcontainer, sourcecontainer, conmodel, elindex, targetmodel, dropindex) {
-                    e.stopPropagation();
-                     $state.go($scope.detailsState + '.activities', {
-                         id: $scope.folderId,
-                         entity: context.entityName,
-                         entityId: context.entityId
-                     }, { reload: false });
-
-                    orderService.setOrder(e, elindex, dropindex, $scope.folders.length - 1);
-                });
-
-                // $scope.$on('dragularrelease', function (e, el) {
-                //     e.stopPropagation();
-                //     $state.go($scope.detailsState + '.activities', {
-                //         id: $scope.folderId,
-                //         entity: context.entityName,
-                //         entityId: context.entityId
-                //     }, { reload: false });
-                // });
-
-                registerEvents(topBar, scroll, -4);
-                registerEvents(buttomBar, scroll, 4);
-
-                function registerEvents(bar, container, inc, speed) {
-                    if (!speed) {
-                        speed = 20;
-                    }
-                    angular.element(bar).on('dragularenter', function () {
-                        container[0].scrollTop += inc;
-                        timer = $interval(function moveScroll() {
-                            container[0].scrollTop += inc;
-                        }, speed);
-                    });
-                    angular.element(bar).on('dragularleave dragularrelease', function () {
-                        $interval.cancel(timer);
-                    });
-                }
-        };
             $scope.context = context;
             $scope.isLoading = true;
 
-            $scope.subfolders = getAllSubfolders();
-
             $scope.folders = concatAllfolders();
-
-            _($scope.folders).each(function (t) {
-                t.__state = creatingStatuses.Created;
-                t.PartTitle = t.title;
-            });
-
-            if (!$scope.displayOnly) {
-                if (context.entityName === 'my') {
-                    UsersService.getMe().then(function (me) {
-                        newFolder.assign = me._id;
-                        $scope.folders.push(_(newFolder).clone());
-                    });
-                } else {
-                    delete newFolder.assign;
-                    if (context.entityName === 'folder') {
-                        // newFolder.parent = context.entity._id;
-                        $scope.folders.push(_(newFolder).clone());
-                    } else
-                        $scope.folders.push(_(newFolder).clone());
-                }
-            }
-
 
             if (context.entityName === 'all') {
                 $scope.detailsState = 'main.folders.all.details';
@@ -114,20 +28,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
                 $scope.detailsState = 'main.folders.byentity.details';
             }
 
-            function getAllSubfolders() {
-                var subfolders = [];
-                $scope.folders.forEach(function (item) {
-                    if (item.subfolders && (item.subfolders.length > 0)) {
-                        return subfolders = subfolders.concat(
-                            item.subfolders.filter(function (subTask) {
-                                return (subTask !== 'undefined');
-                            })
-                        )
-                    }
-                });
-                return subfolders;
-            }
-
             function concatAllfolders() {
               // Change form Array.concat to Array.forEach
               // to fix binding of $scope.folders
@@ -136,87 +36,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
               });
               return $scope.folders;
             }
-
-            $scope.createOrUpdate = function (folder) {
-                if (context.entityName !== 'all') {
-                    folder[context.entityName] = context.entity;
-                }
-
-                if (folder.__state === creatingStatuses.NotCreated) {
-                    folder.__state = creatingStatuses.Creating;
-
-                    return FoldersService.create(folder).then(function (result) {
-                        folder.__state = creatingStatuses.Created;
-
-                        $scope.folders.push(_(newTask).clone());
-
-                        FoldersService.data.push(folder);
-
-                        return folder;
-                    });
-                } else if (folder.__state === creatingStatuses.Created) {
-
-                    folder.title = folder.PartTitle;
-
-                    return FoldersService.update(folder);
-                }
-            };
-            $scope.debouncedUpdate = _.debounce($scope.createOrUpdate, 1);
-
-            $scope.searchResults = [];
-
-            $scope.search = function (folder) {
-                if (context.entityName !== 'discussion') {
-                    return;
-                }
-
-                if (!folder.__autocomplete) {
-                    return;
-                }
-
-                var term = folder.title;
-                if (!term) {
-                    return;
-                }
-
-                $scope.searchResults.length = 0;
-                $scope.selectedSuggestion = 0;
-                FoldersService.search(term).then(function (searchResults) {
-                    _(searchResults).each(function (sr) {
-                        var alreadyAdded = _($scope.folders).any(function (t) {
-                            return t._id === sr._id;
-                        });
-
-                        if (!alreadyAdded) {
-                            $scope.searchResults.push(sr);
-                        }
-                    });
-                    $scope.selectedSuggestion = 0;
-                });
-
-            };
-
-            $scope.select = function (selectedFolder) {
-                var currentFolder = _($scope.folders).findIndex(function (t) {
-                    return t.id === $state.params.id;
-                });
-
-                // FoldersService.remove(currentFolder._id);
-
-                // _(currentFolder).assign(selectedFolder);
-                // currentFolder.__autocomplete = false;
-
-                // $scope.searchResults.length = 0;
-                // $scope.selectedSuggestion = 0;
-
-                $scope.createOrUpdate($scope.folders[currentFolder + 1]).then(function (folder) {
-                    $state.go($scope.detailsState, {
-                        id: folder._id,
-                        entity: context.entityName,
-                        entityId: context.entityId
-                    });
-                });
-            };
 
         }
 
@@ -229,7 +48,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
                 }
 
                 var nameFocused = angular.element($event.target).hasClass('name');
-                folder.PartTitle = folder.title;
 
                 if (folder.__state === creatingStatuses.NotCreated) {
                     $scope.createOrUpdate(folder).then(function () {
@@ -266,58 +84,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
 
                 return isActive;
             };
-
-            $scope.onEnter = function ($event, index) {
-                if ($event.keyCode === 13 || $event.keyCode === 9) {
-                    $event.preventDefault();
-
-                    $scope.folders[index].__autocomplete = false;
-                    if ($element.find('td.name')[index + 1]) {
-                        $element.find('td.name')[index + 1].focus();
-                    }
-                    else {
-                        $timeout(function () {
-                            $element.find('td.name')[index + 1].focus();
-                        }, 500);
-                    }
-
-                }
-            };
-
-            $scope.focusAutoComplete = function ($event) {
-                angular.element($event.target).css('box-shadow', 'none')
-                if ($event.keyCode === 38) {
-                    if ($scope.selectedSuggestion > 0) {
-                        $scope.selectedSuggestion -= 1;
-                    }
-                    $event.preventDefault();
-                } else if ($event.keyCode === 40) {
-                    if ($scope.selectedSuggestion < $scope.searchResults.length - 1) {
-                        $scope.selectedSuggestion += 1;
-                    }
-                    $event.preventDefault();
-                } else if ($event.keyCode === 13 || $event.keyCode === 9) {
-                    var sr = $scope.searchResults[$scope.selectedSuggestion];
-                    $scope.select(sr);
-                }
-
-
-            };
-
-            $scope.hideAutoComplete = function (folder) {
-
-                folder.PartTitle = folder.title;
-
-                folder.__autocomplete = false;
-                $scope.searchResults.length = 0;
-                $scope.selectedSuggestion = 0;
-            };
-
-            // infinite scroll
-            $timeout(function () {
-                $scope.displayLimit = Math.ceil($element.height() / 50);
-                $scope.isLoading = false;
-            }, 0);
 
             $scope.loadMore = function () {
                 if (!$scope.isLoading && $scope.loadNext) {
