76f1d85068e915e9bd24263d06967d0983d3f3b5
manmorc <makeiTEasysoft@mail.ru>
2018-07-15 12:12:35 +0300

    removed unused code

--- packages/custom/icu/public/components/folder-list-directive/folder-list.directive.js
+++ packages/custom/icu/public/components/folder-list-directive/folder-list.directive.js
@@ -19,16 +19,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
                 t.__state = creatingStatuses.Created;
                 t.PartTitle = t.title;
             });
-
-            if (context.entityName === 'all') {
-                $scope.detailsState = 'main.folders.all.details';
-            } else if (context.entityName === 'my') {
-                $scope.detailsState = 'main.folders.byassign.details';
-            } else if (context.entityName === 'folder') {
-                $scope.detailsState = 'main.folders.byparent.details';
-            } else {
-                $scope.detailsState = 'main.folders.byentity.details';
-            }
         }
 
         function link($scope, $element) {
@@ -46,39 +36,6 @@ angular.module('mean.icu.ui.folderlistdirective', ['dragularModule'])
                 }
 
                 return isActive;
-            };
-
-
-            // infinite scroll
-            $timeout(function () {
-                $scope.displayLimit = Math.ceil($element.height() / 50);
-                $scope.isLoading = false;
-            }, 0);
-
-            $scope.loadMore = function () {
-                if (!$scope.isLoading && $scope.loadNext) {
-                    $scope.isLoading = true;
-                    $scope.loadNext().then(function (folders) {
-
-                        _(folders.data).each(function(t) {
-                            t.__state = creatingStatuses.Created;
-                            t.PartTitle = t.title;
-                        });
-
-                        var offset = $scope.displayOnly ? 0 : 1;
-
-                        if (folders.data.length) {
-                            var index = $scope.folders.length - offset;
-                            $scope.folders.pop();
-                            var args = [index, 0].concat(folders.data);
-                            [].splice.apply($scope.folders, args);
-                        }
-
-                        $scope.loadNext = folders.next;
-                        $scope.loadPrev = folders.prev;
-                        $scope.isLoading = false;
-                    });
-                }
             };
         }
 
