50ab67fd2c6d405703df6bb8a2c390b02ca267e7
Omer Zamir <zamir98@gmail.com>
2018-07-15 14:43:18 +0300

    removed unnecessary parameters from model
    discussion model - elasticsearch delete and save functions

--- packages/custom/icu/server/models/discussion.js
+++ packages/custom/icu/server/models/discussion.js
@@ -129,7 +129,7 @@ DiscussionSchema.post('save', function() {
   elasticsearch.save(this, 'discussion');
 });
 DiscussionSchema.pre('remove', function(next) {
-  elasticsearch.delete(this, 'discussion', null, next);
+  elasticsearch.delete(this, 'discussion', next);
   next();
 });
 
