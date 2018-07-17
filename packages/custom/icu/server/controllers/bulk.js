e05328c30370cbc8af59b952a5dcd138950523e7
Nevos <nevonat@gmail.com>
2018-07-17 13:07:34 +0300

    added bulk permissions and elastic sync

--- packages/custom/icu/server/controllers/bulk.js
+++ packages/custom/icu/server/controllers/bulk.js
@@ -1,5 +1,6 @@
 const httpError = require('http-errors');
-var q = require('q');
+const permissions = require('../controllers/permissions.js');
+var elasticsearch = require('../controllers/elasticsearch');
 
 const models = {
   task: require('../models/task'),
@@ -35,8 +36,13 @@ function update(req, res, next) {
   tags = tags || [];
 
   Model.find({ _id: { $in: ids } })
-  .then(docs => checkBoldedPermissions(docs) ? true : next("perms Err"))
+  .then(docs => docs.length == 0 ? next() : docs )
+  .then(docs => {
+    if (checkBoldedPermissions(docs,req.user)) return docs ;
+    else throw new Error("Permission Denied") ;
+  })
   .then(function(docs) {
+//    console.log("bulk res", docs) ;
     if(!docs.length) throw new httpError(404);
     return Model.update({
       _id: { $in: ids }
@@ -56,25 +62,40 @@ function update(req, res, next) {
   })
   .then(function(results) {
     return Model.find({ _id: { $in: ids } })
-  })
+            .then(docs => {
+                docs.forEach(element => {
+                console.log("saving elastic") ;                
+                element.watchers = [];
+                elasticsearch.save(element, entity);
+                }) 
+                return docs ;
+            })
+  })  
   .then(function(docs) {
     if(!docs.length) throw new httpError(404);
     res.json(docs)
-  })
+  })  
   .catch(function(err) {
     next(err)
   })
 }
 
-function checkBoldedPermissions(docs) {
+function checkBoldedPermissions(docs,user) {
+  let permissionDenied = docs.some((doc) => {
+    let allowed = permissions.updateContent({user: user},doc, []) ;    
+    if(!allowed) {
+      console.log("checkBoldedPermissions NOT ALLLOWED") ;
+      return !allowed ;       
+    }
+  }) ;
 
-
-  console.log("checkBoldedPermissions", docs) ;
-  if(docs.length) {
+  if (permissionDenied) {
     return false ;
   }
-  
+
+  return true ;  
 }
+
 
 function recycle(req, res, next) {
   let entity = req.params.entity;
