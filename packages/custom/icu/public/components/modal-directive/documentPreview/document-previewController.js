0deeb1340516418d71f92cdbae3c22d8ba72bc43
sraya avraham <sraya305@gmail.com>
2018-07-26 15:12:35 +0300

    fixed preview with ftp

--- packages/custom/icu/public/components/modal-directive/documentPreview/document-previewController.js
+++ packages/custom/icu/public/components/modal-directive/documentPreview/document-previewController.js
@@ -1,4 +1,4 @@
-function controllerDocumentPreview($scope, $uibModalInstance, attachment, $filter,$http, UsersService,AttachmentsService) {
+function controllerDocumentPreview($scope, $uibModalInstance, attachment, $filter,$http, UsersService, AttachmentsService, DocumentsService) {
 
     $scope.path = "" ;
 
@@ -19,12 +19,36 @@ function controllerDocumentPreview($scope, $uibModalInstance, attachment, $filte
         .then(user => $scope.attCreator = user.name);
 
     $scope.attPath =  attachment.path ;
+    $scope.attForFTPPath = $scope.attPath.substring($scope.attPath.indexOf("/files"),$scope.attPath.length);
+    $scope.attForFTPPath  = $scope.attForFTPPath.replace(/\//g, '%2f');
     $scope.attPrint =  attachment.size ;
     $scope.attType =  attachment.attachmentType;
 
-    $scope.previewWindow = function(document) {
-        AttachmentsService.previewWindow(document);
-    }
+    $scope.previewWindow = function (document1) {
+        if (document1.documentType == "docx" || document1.documentType == "doc" || document1.documentType == "xlsx" || document1.documentType == "xls" || document1.documentType == "ppt" || document1.documentType == "pptx") {
+         var pdfPath = document1.path.replace("/files","/preview");
+
+         pdfPath = pdfPath.substring(pdfPath.indexOf('preview'),pdfPath.lastIndexOf('.'));
+         pdfPath = pdfPath+".pdf";
+         pdfPath = pdfPath.replace(/\//g,"%2f");
+         console.log(pdfPath);
+         DocumentsService.viewFileLink(pdfPath).then(function(result){
+             window.open(result + '?view=true');
+             console.dir($scope.path);
+         });
+
+     }
+     else{
+         var picPath = document1.path;
+
+         picPath = picPath.substring(picPath.indexOf('files'),picPath.length);
+         picPath = picPath.replace(/\//g,"%2f");
+         DocumentsService.viewFileLink(picPath).then(function(result){
+             window.open(result + '?view=true');
+         });
+
+     }
+ };
 
 
     $scope.previewAttachment = function () {
@@ -36,6 +60,33 @@ function controllerDocumentPreview($scope, $uibModalInstance, attachment, $filte
 
     $scope.previewAttachment() ;
 
+    function myTest(document1) {
+
+        if (document1.documentType == "docx" || document1.documentType == "doc" || document1.documentType == "xlsx" || document1.documentType == "xls" || document1.documentType == "ppt" || document1.documentType == "pptx" || document1.documentType == "tif") {
+            var pdfPath = document1.path.replace("/files","/preview");
+
+            pdfPath = pdfPath.substring(pdfPath.indexOf('preview'),pdfPath.lastIndexOf('.'));
+            pdfPath = pdfPath+".pdf";
+            pdfPath=pdfPath.replace(/\//g,"%2f");
+            console.log(pdfPath);
+            DocumentsService.viewFileLink(pdfPath,document1.documentType).then(function(result){
+                $scope.path =  result + '?view=true';
+            });
+
+        }
+        else{
+            var pdfPath = document1.path;
+
+            pdfPath = pdfPath.substring(pdfPath.indexOf('files'),pdfPath.length);
+            pdfPath = pdfPath.replace(/\//g,"%2f");
+            DocumentsService.viewFileLink(pdfPath).then(function(result){
+                $scope.path =  result + '?view=true';
+            });
+
+        }
+    }
+
+/**
     function myTest(document1) {
         // Check if need to view as pdf
         if ((document1.documentType == "docx") ||
@@ -69,7 +120,7 @@ function controllerDocumentPreview($scope, $uibModalInstance, attachment, $filte
             $scope.path = document1.path + '?view=true' ;
         }
     }
-
+ */
     $scope.ok = function (sendingForm) {
         $uibModalInstance.dismiss('cancel');
     };
