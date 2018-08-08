eae61466dbd940218712b7fc511b39d91e8d65c1
Nevos <nevonat@gmail.com>
2018-08-08 16:19:34 +0300

    rmoved logs / tabs

--- packages/custom/icu/server/models/modelUtils.js
+++ packages/custom/icu/server/models/modelUtils.js
@@ -1,28 +1,25 @@
-var userSchema = require('./user') ;
+var userSchema = require('./user');
 
-function superSeeAll(entity,next) {
-    console.log("SUPER SEE ALL") ;
+function superSeeAll(entity, next) {
     userSchema.find({})
-    .then(docs => {
-
-    console.log("TASK UPDATED0:", entity) ;
-    entity.watchers = [] ;
-    docs.forEach(doc => entity.watchers.push(doc._id) );
-    entity.permissions = superSeeAllPerms(entity.watchers) ;
-    next();
-    }) ;  
-} 
+        .then(docs => {
+            entity.watchers = [];
+            docs.forEach(doc => entity.watchers.push(doc._id));
+            entity.permissions = superSeeAllPerms(entity.watchers);
+            next();
+        });
+}
 
 function superSeeAllPerms(newWatchers) {
-    let newPerms = [] ;
-    if(newWatchers.length > 0) {
-        newWatchers.forEach(function(watcher) {
-            let permLevel = 'editor' ;
-            let watcherAddedPerms = {id:String(watcher),level:permLevel} ; 
+    let newPerms = [];
+    if (newWatchers.length > 0) {
+        newWatchers.forEach(function (watcher) {
+            let permLevel = 'editor';
+            let watcherAddedPerms = { id: String(watcher), level: permLevel };
             newPerms.push(watcherAddedPerms);
-    })
-    }                        
-    return newPerms ;
+        })
+    }
+    return newPerms;
 }
 
 module.exports = {
