29a45adbbd92a7e34fe9b4d4dea41e64d00d60a2
Omer Zamir <zamir98@gmail.com>
2018-09-13 15:43:02 +0300

    fix bug
    there was a comma instead of +

--- packages/custom/icu/public/components/office-details/office-details.js
+++ packages/custom/icu/public/components/office-details/office-details.js
@@ -79,7 +79,7 @@ function OfficeDetailsController($rootScope, $scope, entity, tasks, folders, peo
     OfficesService.WantToCreateRoom($scope.item).then(function(data) {
       navigateToDetails($scope.item);
       if(data.roomName) {
-        $window.open(window.config.rocketChat.uri + '/group/', data.roomName);
+        $window.open(window.config.rocketChat.uri + '/group/' + data.roomName);
         return true;
       }
       else {
