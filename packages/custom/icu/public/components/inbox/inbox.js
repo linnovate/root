1f06df8503870cd743391cf603c1ef788fb3bd34
manmorc <makeiTEasysoft@mail.ru>
2018-09-25 14:58:44 +0300

    fixed controller bug

--- packages/custom/icu/public/components/inbox/inbox.js
+++ packages/custom/icu/public/components/inbox/inbox.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('mean.icu.ui.inbox').controller('InboxListController',
+angular.module('mean.icu.ui.inbox', [])
+.controller('InboxListController',
     function ($scope, $state, $stateParams, me, activities) {
         $scope.me = me;
         $scope.activities = activities;
