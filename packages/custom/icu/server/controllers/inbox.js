fab7f393ea20fdcd558a29152847d9474443a371
manmorc <makeiTEasysoft@mail.ru>
2018-09-27 13:30:21 +0300

    added description to activities actions added showing updated entity in details pane

--- packages/custom/icu/server/controllers/inbox.js
+++ packages/custom/icu/server/controllers/inbox.js
@@ -30,6 +30,8 @@ function getUpdateEntities(req, res, next) {
         Promises.push(
             new Promise( resolve => {
                 return Model.find({ _id: { $in: entities[entityType] } })
+                    .populate('creator')
+                    .populate('userObj')
                     .populate('watchers')
                     .populate('project')
                     .populate('folder')
