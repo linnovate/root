bdc9a7e841ce305394e5798487ff3a7f7c6bc197
artem <artemartem123456123@gmail.com>
2018-11-06 15:53:44 +0300

    create, due, title, description activities

--- packages/custom/icu/public/components/inbox/inbox.js
+++ packages/custom/icu/public/components/inbox/inbox.js
@@ -9,64 +9,20 @@ angular.module('mean.icu.ui.inbox', [])
         $scope.inboxState = 'main.inbox';
 
         $scope.getActivityDescription = (activity) => {
-            let creator = activity.creator.username;
-
-            switch (activity.type){
+            let creator = activity.creator.name;
+            switch (activity.updateField){
                 case 'create' :
                     return `${creator} ${$i18next('created')} ${activity.entityObj.title}`;
                     break;
-                case 'updateWatcher' :
-                    let newWatcher = activity.userObj.name;
-                    return (creator === me._id)
-                        ? `${$i18next('youAddedAsWatcher')} ${newWatcher}`
-                        : `${creator} ${$i18next('addedYouAsWatcher')}`;
+                case 'due' :
+                    return `${creator} ${$i18next('changedDueDateTo')} ${moment(activity.entityObj.due).format('DD/MM/YYYY')}`
                     break;
-                case 'removeWatcher' :
-                    return `${creator} ${$i18next('removeWatcher')} ${activity.userObj.name}`;
-                    break;
-                case 'updateEntity' :
-                    return `${creator} ${$i18next('changedParentOf')} ${activity.entityObj.title} ${$i18next('to')} ${activity.entityObj.title}`;
-                    break;
-                case 'updateNewEntity' :
-                    return `${creator} ${$i18next('bindedEntity')} ${activity.entityObj.title} ${$i18next('with')} ${activity.entity}`;
-                    break;
-                case 'assign' :
-                case 'assignNew' :
-                    return `${creator} ${$i18next('assigned')} ${activity.userObj.name} ${$i18next('to')} ${activity.entityObj.title}`;
-                    break;
-                case 'unassign' :
-                    return `${creator} ${$i18next('unassigned')} ${activity.userObj.name} ${$i18next('from')} ${activity.entityObj.title}`;
-                    break;
-                case 'updateDue' :
-                    return `${creator} ${$i18next('changedDueDateTo')} ${activity.TaskDue}`;
-                    break;
-                case 'comment' :
-                    return `${creator} ${$i18next('leavedComment')} ${activity.description}`;
-                    break;
-                case 'document' :
-                    return `${creator} ${$i18next('addedAttachment')}`;
-                    break;
-                case 'documentDelete' :
-                    return `${creator} ${$i18next('removedAttachment')}`;
-                    break;
-                case 'update' :
-                case 'updateNew' :
-                case 'updateTitle' :
-                case 'updateNewTitle' :
+                case 'title' :
                     return `${creator} ${$i18next('updated')} ${activity.entityObj.title}`;
                     break;
-                case 'updateStatus' :
-                    return activity.status === 'deleted' ?
-                     `${creator} ${$i18next('removed')} ${activity.entityObj.title}` :
-                     `${creator} ${$i18next('changedStatusTo')} ${activity.status}`;
-                    break;
-                case 'updateNewLocation' :
-                    return`${creator} ${$i18next('addedLocation')} ${activity.status}`;
-                    break;
-                case 'updateLocation' :
-                    return activity.status === '' ?
-                     `${creator} ${$i18next('removedLocation')}` :
-                     `${creator} ${$i18next('changedLocationTo')} ${activity.status}`;
+                case 'description' :
+                    debugger
+                    return `${creator} ${$i18next('updated')} ${activity.entityObj.description}`;
                     break;
             }
         };
@@ -81,7 +37,7 @@ angular.module('mean.icu.ui.inbox', [])
             }
         };
 
-        $scope.formatDate = date => moment(date).format('HH:mm | DD/mm/YYYY');
+        $scope.formatDate = date => moment(date).format('HH:mm | DD/MM/YYYY');
 
         $scope.initiate = function($event, entity, activityType) {
             $state.go($scope.inboxState + '.' + activityType, {
