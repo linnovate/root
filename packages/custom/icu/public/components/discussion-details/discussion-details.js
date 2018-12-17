e63be88bf2901af6d8ad3129fb4f28af6b3bc641
artem123456123 <43031140+artem123456123@users.noreply.github.com>
2018-12-17 13:03:24 +0300

    refresh list after entity's status updated (#1427)
    * refresh list after entity's status updated
    
    * Moving `refreshList` into first `then`

--- packages/custom/icu/public/components/discussion-details/discussion-details.js
+++ packages/custom/icu/public/components/discussion-details/discussion-details.js
@@ -343,6 +343,7 @@ function DiscussionDetailsController($scope, $rootScope, entity, tasks, context,
         backupEntity = JSON.parse(JSON.stringify($scope.item));
         ActivitiesService.data = ActivitiesService.data || [];
         ActivitiesService.data.push(result);
+        refreshList();
       });
       break;
 
