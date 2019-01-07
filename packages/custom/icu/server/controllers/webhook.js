c5f6e2143d9437fdcce26340f7adf1cd5d2f6e19
Yitzchak Acker <yitzchak@linnovate.net>
2019-01-07 15:01:54 +0200

    fixed storing subTasks from template mapping to ids

--- packages/custom/icu/server/controllers/webhook.js
+++ packages/custom/icu/server/controllers/webhook.js
@@ -61,15 +61,20 @@ exports.subTasks = function(req, res, next) {
       return next();
     }
     Task.update({
-      _id: task._id}, {$addToSet: {
-      subTasks: data
-    }}).exec(function(err, res) {
-      if (err) req.locals.error = err;
-      else {
-        req.locals.result = task;
-        req.locals.result.subTasks = data;
-      }
-      next();
-    });
+      _id: task._id
+    }, {
+        $addToSet: {
+          subTasks: {
+            $each: data.map(s => s._id)
+          }
+        }
+      }).exec(function (err, res) {
+        if (err) req.locals.error = err;
+        else {
+          req.locals.result = task;
+          req.locals.result.subTasks = data;
+        }
+        next();
+      });
   });
 }
