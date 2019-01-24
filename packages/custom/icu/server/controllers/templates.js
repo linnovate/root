3c84fac7adf2d3361368a7ae1557c05267ab3109
Yitzchak Acker <yitzchak@linnovate.net>
2019-01-24 18:46:56 +0200

    grant template creator an editor permission to template object and to subtasks that are automatically added by template as project policy

--- packages/custom/icu/server/controllers/templates.js
+++ packages/custom/icu/server/controllers/templates.js
@@ -72,15 +72,15 @@ var addUpdate = function(taskId, creator, type, description, callback) {
     created: new Date(),
     updated: new Date()
   });
-  update.save(function (err, update) {
-    if (callback) {
+  update.save(function(err, update) {
+    if(callback) {
       callback(err, update);
     }
   });
 };
 
-var addToTemplate = function (task, due, parentId, name, creator, watchers, circles, project, exist, tType, office, folder, custom, callback) {
-  if (!exist) {
+var addToTemplate = function(task, due, parentId, name, creator, watchers, permissions, circles, project, exist, tType, office, folder, custom, callback) {
+  if(!exist) {
     var template = new Task({
       tType: tType,
       title: name || task.title,
@@ -89,6 +89,7 @@ var addToTemplate = function (task, due, parentId, name, creator, watchers, circ
       tags: task.tags,
       description: task.description,
       watchers: watchers,
+      permissions: permissions,
       circles: circles,
       project: project,
       office: office,
@@ -120,16 +121,16 @@ var addToTemplate = function (task, due, parentId, name, creator, watchers, circ
   }
 };
 
-var addRecorsiveTemplates = function (taskId, name, parentId, creator, watchers, circles, project, exist, tType, templates, totals, created, office, folder, customId, callback) {
+var addRecorsiveTemplates = function (taskId, name, parentId, creator, watchers, permissions, circles, project, exist, tType, templates, totals, created, office, folder, customId, callback) {
   Task.findOne({
     _id: taskId
   })
     .populate('subTasks')
-    .exec(function (err, task) {
-      if (task) {
+    .exec(function(err, task) {
+      if(task) {
         totals.tasks += task.subTasks.length;
         var due = task.due;
-        if (!tType && task.due) {
+        if(!tType && task.due) {
           var timeDiff = Math.abs(task.due.getTime() - created.getTime());
           var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
           var date = new Date();
@@ -137,79 +138,74 @@ var addRecorsiveTemplates = function (taskId, name, parentId, creator, watchers,
           due = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0);
         }
 
-        let custom;
-        if (task.custom && task.custom.id) custom = task.custom;
-        else if (customId && task.tType === 'template' && parentId != null) {
-          let generatedCustomId = customId + task.title.replace(/\s+/g, "").toLowerCase(),
-            generatedCustomType = 'subTask';
-          const idx = task.tags.findIndex(t => t.includes('type:'));
-          if (idx !== -1) {
-            const tag = task.tags[idx];
-            const valArr = tag.split(':');
-            if (valArr.length > 0) generatedCustomType = valArr[1];
-            task.tags.splice(idx, 1);
+         let custom;
+         if (task.custom && task.custom.id) custom = task.custom;
+         else if (customId && task.tType === 'template' && parentId != null) {
+           let generatedCustomId = customId + task.title.replace(/\s+/g, "").toLowerCase(),
+             generatedCustomType = 'subTask';
+           const idx = task.tags.findIndex(t => t.includes('type:'));
+           if (idx !== -1) {
+             const tag = task.tags[idx];
+             const valArr = tag.split(':');
+             if (valArr.length > 0) generatedCustomType = valArr[1];
+             task.tags.splice(idx, 1);
           }
-
-          custom = {
-            id: generatedCustomId,
-            type: generatedCustomType
-          }
-        }
-        addToTemplate(task, due, parentId, name, creator, watchers, circles, project, exist, tType, office, folder, custom, function (template) {
-          let customIdExists = false;
-          if (template.t.custom && template.t.custom.id && template.t.custom.type) {
-            const findIdxPredicate = t => {
-              return t.custom && t.custom.id && t.custom.type &&
-                t.custom.id === template.t.custom.id && t.custom.type === template.t.custom.type;
-            }
-            let idx = Object.values(templates).findIndex(findIdxPredicate);
-            if (idx !== -1) {
-              customIdExists = true;
-            }
-            if (parentId != null && templates[parentId] && templates[parentId].subTasks) {
-              idx = templates[parentId].subTasks.findIndex(findIdxPredicate);
-              if (idx !== -1) {
-                customIdExists = true;
-              }
-            }
-          }
-
-          let templateId = {
-            _id: template.t._id
-          };
-          if (template.t.custom && template.t.custom.id && template.t.custom.type) {
-            templateId.custom = {
-              id: template.t.custom.id,
-              type: template.t.custom.type
-            };
-          }
-
-          if (customId == null || !customIdExists) {
-            templates[template.t._id] = template;
-            if (parentId) {
-              templates[parentId].t.subTasks.push(template.t._id);
-              if(!templates[parentId].subTasks) templates[parentId].subTasks = [];
-              templates[parentId].subTasks.push(templateId);
-            }
-            totals.templates++;
-          }
-          if (totals.templates === totals.tasks) {
-            return returnTemplates();
+ 
+           custom = {
+             id: generatedCustomId,
+             type: generatedCustomType
+           }
+         }
+         addToTemplate(task, due, parentId, name, creator, watchers, permissions, circles, project, exist, tType, office, folder, custom, function (template) {
+           let customIdExists = false;
+           if (template.t.custom && template.t.custom.id && template.t.custom.type) {
+             const findIdxPredicate = tmplt => {
+               return tmplt.t.custom && tmplt.t.custom.id && tmplt.t.custom.type &&
+                 tmplt.t.custom.id === template.t.custom.id && tmplt.t.custom.type === template.t.custom.type;
+             }
+             let idx = Object.values(templates).findIndex(findIdxPredicate);
+             if (idx !== -1) {
+               customIdExists = true;
+             }
+           }
+ 
+           let templateId = {
+             _id: template.t._id
+           };
+           if (template.t.custom && template.t.custom.id && template.t.custom.type) {
+             templateId.custom = {
+               id: template.t.custom.id,
+               type: template.t.custom.type
+             };
+           }
+ 
+           if (customId == null || !customIdExists) {
+             templates[template.t._id] = template;
+             if (parentId) {
+               templates[parentId].t.subTasks.push(template.t._id);
+               if(!templates[parentId].subTasks) templates[parentId].subTasks = [];
+               templates[parentId].subTasks.push(templateId);
+             }
+             totals.templates++;
+           }
+           if (totals.templates === totals.tasks) {
+             return returnTemplates();
           }
           watchers = template.t.watchers;
+          permissions = template.t.permissions;
           circles = template.t.circles;
           project = template.t.project;
           office = template.t.office;
           folder = template.t.folder;
 
           for (var i = 0; i < task.subTasks.length; i++) {
-            addRecorsiveTemplates(task.subTasks[i]._id, null, template.t._id, creator, watchers, circles, project, false, tType, templates, totals, created, office, folder, customId, callback);
+            addRecorsiveTemplates(task.subTasks[i]._id || task.subTasks[i], null, template.t._id, creator, watchers, permissions, circles, project, false, tType, templates, totals, created, office, folder, customId, callback);
           }
         });
       }
       else {
         totals.templates++;
-        if (totals.templates === totals.tasks) {
+        if(totals.templates === totals.tasks) {
           return returnTemplates();
         }
       }
@@ -219,14 +215,14 @@ var addRecorsiveTemplates = function (taskId, name, parentId, creator, watchers,
     templates = Object.values(templates).map(template => {
       return {
        t: template.t,
-       s: template.s 
+       s: template.s
       }
     });
     return callback(templates);
   }
 };
 
-exports.toTemplate = function (req, res, next) {
+exports.toTemplate = function(req, res, next) {
   //TODO: save documents
   var templates = {},
     totals = {
@@ -240,25 +236,26 @@ exports.toTemplate = function (req, res, next) {
       $ne: 'template'
     }
   })
-    .exec(function (err, task) {
-      if (err) {
+    .exec(function(err, task) {
+      if(err) {
         req.locals.error = err;
       }
       else {
         totals.tasks = 1;
         var watchers = req.body.watchers || [req.body.watcher];
+        var permissions = req.body.permissions || [req.body.permission];
         var circles = req.body.circles;
-        addRecorsiveTemplates(req.params.id, req.body.name, null, req.user._id, watchers, circles, null, false, 'template', templates, totals, null, null, null, null, function (templates) {
+        addRecorsiveTemplates(req.params.id, req.body.name, null, req.user._id, watchers, permissions, circles, null, false, 'template', templates, totals, null, null, null, null, function (templates) {
           var counter = Object.keys(templates).length;
-          for (var t in templates) {
-            templates[t].t.save(function (err, subtask) {
-              if (subtask.templateId.toString() === task._id.toString()) {
+          for(var t in templates) {
+            templates[t].t.save(function(err, subtask) {
+              if(subtask.templateId.toString() === task._id.toString()) {
                 req.locals.result = subtask;
                 next();
               }
               counter--;
-              if (counter === 0) {
-                for (var t in templates) {
+              if(counter === 0) {
+                for(var t in templates) {
                   cloneAttachments(templates[t].t.templateId, t, req.user._id, watchers, circles);
                 }
               }
@@ -297,27 +294,27 @@ exports.toSubTasks = function(req, res, next) {
         tType: {
           $ne: 'template'
         }
-      }).exec(function (err, task) {
-        if (err) {
+      }).exec(function(err, task) {
+        if(err) {
           req.locals.error = err;
-          if (cb) return cb(err); else next();
+          if(cb) return cb(err); else next();
         }
         else {
           totals.tasks = 1;
-          addRecorsiveTemplates(req.params.id, req.body.name, null, req.user._id, [], {}, null, req.body.taskId, null, tasks, totals, created, null, null, req.body.custom.id, function (templates) {
+          addRecorsiveTemplates(req.params.id, req.body.name, null, req.user._id, [], [], {}, null, req.body.taskId, null, tasks, totals, created, null, null, req.body.custom.id, function (templates) {
             req.locals.result = [];
-            for (var t in templates) {
-              templates[t].t.save(function (err, subtask) {
-                if (typeof subtask != 'undefined') { // TBD - need to check why we have empties
+            for(var t in templates) {
+              templates[t].t.save(function(err, subtask) {
+                if(typeof subtask != 'undefined') { // TBD - need to check why we have empties
                   addUpdate(subtask._id, req.user, 'copy');
                   cloneAttachments(subtask.templateId, subtask._id, req.user._id, subtask.watchers, subtask.circles);
                 }
               });
-              if (templates[t].t.parent && templates[t].t.parent.toString() === req.body.taskId.toString()) {
+              if(templates[t].t.parent && templates[t].t.parent.toString() === req.body.taskId.toString()) {
                 req.locals.result.push(templates[t].t);
               }
             }
-            if (cb) return cb(null, req.locals.result); else next();
+            if(cb) return cb(null, req.locals.result); else next();
           });
         }
       });
