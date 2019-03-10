1bc4bd92c7019618dccde0df1ac06002b0885438
Avraham Ostreicher <avraham@linnovate.net>
2019-03-10 13:19:30 +0200

    Cleanup wrong function `utils.checkAndHandleError`

--- packages/custom/icu/server/controllers/utils.js
+++ packages/custom/icu/server/controllers/utils.js
@@ -2,13 +2,6 @@
 
 var inspect = require('util').inspect;
 
-exports.checkAndHandleError = function(err, defaultMessage, next) {
-  if(err) {
-    var errMesg = defaultMessage || err.errors || err.message || 'Oops...';
-    next(new Error(errMesg));
-  }
-};
-
 exports.errorHandler = function(err, req, res, next) {
   next(err);
 };
