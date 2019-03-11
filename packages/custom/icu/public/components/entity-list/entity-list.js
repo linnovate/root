c024d09be148636b623c857d8232aa3f2d196b60
Avraham Ostreicher <avraham@linnovate.net>
2019-03-11 12:24:55 +0200

    Fix update issue - caused by wrong date value

--- packages/custom/icu/public/components/entity-list/entity-list.js
+++ packages/custom/icu/public/components/entity-list/entity-list.js
@@ -439,7 +439,7 @@ function EntityListController($scope, $window, $state, context, $filter, $stateP
         // if($stateParams.entity)newArray = filterByParent(newArray, $stateParams.entityId);
 
         if($scope.sorting.field.value === 'created')
-            newArray = newArray.map(entity => entity.created = new Date(entity.created) && entity);
+            newArray.forEach(entity => entity.created = new Date(entity.created));
         newArray = $filter('orderBy')(newArray, $scope.sorting.field.value, $scope.sorting.isReverse);
 
         return newArray;
