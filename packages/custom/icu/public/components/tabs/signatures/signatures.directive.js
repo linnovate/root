92c2d4798e4144e4e0db0c3ca3c330df2e2ed8fb
Avraham Ostreicher <avraham@linnovate.net>
2019-05-22 12:37:58 +0300

    Fix deleting signatures - #1573

--- packages/custom/icu/public/components/tabs/signatures/signatures.directive.js
+++ packages/custom/icu/public/components/tabs/signatures/signatures.directive.js
@@ -19,7 +19,10 @@ angular.module('mean.icu.ui.tabs')
             }
             $scope.delete = function(signature){
                 SignaturesService.remove(signature._id).then(function(result){
-                    $scope.signatures.pop(result)
+                    let index = $scope.signatures.findIndex(sig => sig._id === signature._id);
+                    if(index != -1) {
+                        $scope.signatures.splice(index, 1);
+                    }
                 }).catch(function(error){
 
                 })
